b8006c09895b2ed522218854fa44adef6b753da3
alexkernphysiker <kernphysiker@openmailbox.org>
2015-01-19 13:03:49 +0100

    use make instead of qmake

--- autoconf.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-(git submodule init;git submodule update)
-echo ROOTDEPENDPATH = $ROOTSYS/lib$'\n'ROOTINCLUDEPATH = $ROOTSYS/include$'\n'ROOTSORTERPATH = $ROOTSORTERSYS > qt_build.pri
--- buildme.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-(cd Analysis_test;qmake Analysis_test.pro;make clean; make; rootcint -f DictOutput.cxx -c -llibAnalyser.so analysisjob.hh LinkDef.hh)
