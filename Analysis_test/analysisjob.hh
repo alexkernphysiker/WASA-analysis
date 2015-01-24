d429622a1f47f233794638a3ff8472662832cffa
alexkernphysiker <kernphysiker@openmailbox.org>
2015-01-24 17:33:14 +0100

    Tabify

--- Analysis_test/analysisjob.hh
+++ Analysis_test/analysisjob.hh
@@ -13,12 +13,19 @@
 #include <TH1.h>
 #include <TH2.h>
 #include "TCutG.h"
+const Double_t m_target=1.875613;	//[GeV]
+const Double_t m_beam=1.875613;		//[GeV]
+const Double_t m_3He=2.808950;		//[GeV]
+const Double_t m_p=0.938272;		//[GeV]
+const Double_t m_n=0.93956;		//[GeV]
+const Double_t m_4He=3.7264225;		//[GeV]
+const Double_t m_eta=0.547853;		//[GeV]
 enum TrackTypes {kFDN=1,kFDC=2,kCDN=11,kCDC=12};//FD neutral, FD charged, CD neutral, CD charged
 enum ParticleTypes{kDummy=0,kGamma=1,kElectron=2,kPositron=3,kPi0=7,kPiPlus=8,kPiMinus=9,kNeutron=13,kProton=14,kDeuteron=45,kTriton=46,kHe3=49};
 enum ForwardDetectorPlanes{kFWC1=10,kFWC2=11,kFTH1=1,kFTH2=2,kFTH3=3,kFRH1=4,kFRH2=5,kFRH3=6,kFRH4=7,kFRH5=8,kFVH=9};
 class AnalysisJob : public CAnalysisModule{
 public:
-	AnalysisJob();
+AnalysisJob();
 	explicit AnalysisJob(const char * name);
 	virtual ~AnalysisJob();
 	virtual void ProcessEvent();
