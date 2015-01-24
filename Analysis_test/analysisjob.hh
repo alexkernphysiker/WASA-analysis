258dcefc7b65f275f2a2efe06c4fa3d670812711
alexkernphysiker <kernphysiker@openmailbox.org>
2015-01-24 02:59:11 +0100

    header file. fields added

--- Analysis_test/analysisjob.hh
+++ Analysis_test/analysisjob.hh
@@ -13,6 +13,9 @@
 #include <TH1.h>
 #include <TH2.h>
 #include "TCutG.h"
+enum TrackTypes {kFDN=1,kFDC=2,kCDN=11,kCDC=12};//FD neutral, FD charged, CD neutral, CD charged
+enum ParticleTypes{kDummy=0,kGamma=1,kElectron=2,kPositron=3,kPi0=7,kPiPlus=8,kPiMinus=9,kNeutron=13,kProton=14,kDeuteron=45,kTriton=46,kHe3=49};
+enum ForwardDetectorPlanes{kFWC1=10,kFWC2=11,kFTH1=1,kFTH2=2,kFTH3=3,kFRH1=4,kFRH2=5,kFRH3=6,kFRH4=7,kFRH5=8,kFVH=9};
 class AnalysisJob : public CAnalysisModule{
 public:
 	AnalysisJob();
@@ -23,9 +26,17 @@ public:
 	virtual void Print(Option_t *option = "");
 	virtual void UserCommand(CCommand * command);
 private:
-
+	REventHeaderi     *fHeader;
+	WTrackBank        *fTrackBankFD;
+	WTrackBank        *fTrackBankCD;
+	FDFTHTracks       *TrackFinderFD;
+	FDEdep2Ekin       *fFDEdep2Ekin;
+	WVertexBank       *fVertexBank;
+	WTrackBank        *fMCTrackBank;
+	WVertexBank       *fMCVertexBank;
+	CCardWDET         *fDetectorTable;
+	MCTrackFinder     *fMCTrackFinder;
 protected:
 	ClassDef(AnalysisJob,0);
 };
-
 #endif // ANALYSISJOB_H
