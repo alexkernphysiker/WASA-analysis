78ae7b7d6231d72f4c88d1b2da9e27029b79b289
alexkernphysiker <kernphysiker@openmailbox.org>
2015-02-25 16:11:34 +0100

    qmake fix2

--- autoconf.sh
+++ autoconf.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 (git submodule init;git submodule update)
-echo ROOT_DIR=$ROOTSYS$'\n'WASA_DIR=$ROOTSORTERSYS$'\n'LIBS+="$($ROOTSORTERSYS/core/bin/sorter-config -ld -libs-wasa -libs-wasa-ana)" > settings.pri
+WASA_LIBS=`$ROOTSORTERSYS/core/bin/sorter-config -ld -libs-wasa -libs-wasa-ana`
+echo ROOT_DIR=$ROOTSYS$'\n'WASA_DIR=$ROOTSORTERSYS$'\n'LIBS+=$WASA_LIBS > settings.pri
