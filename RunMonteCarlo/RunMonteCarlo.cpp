1d87d8fc6528d9b09ce6e4ed87f559ef8c80a2b2
Rundel <rundel@wfais.uj>
2015-03-10 16:54:17 +0100

    MonteCarlo fix

--- RunMonteCarlo/RunMonteCarlo.cpp
+++ RunMonteCarlo/RunMonteCarlo.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 int main(int, char **){
 	PBeamSmearing *smear = new PBeamSmearing("beam_smear", "Beam smearing");
-	smear->SetReaction("p + d");
+	smear->SetReaction("p+p");
 	smear->SetMomentumFunction(new TF1("Uniform","1",1.426,1.635));
 	makeDistributionManager()->Add(smear);
 	list<string> reactlist;
+	reactlist.push_back("He3 eta");
 	reactlist.push_back("He3 eta [g g]");
 	reactlist.push_back("He3 eta [pi0 pi0 pi0]");
 	for(string react:reactlist){
