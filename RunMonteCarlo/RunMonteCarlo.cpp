4ed19cf4ef7cb794bbe088b44fe288961ce295fc
alexkernphysiker <kernphysiker@openmailbox.org>
2015-03-18 12:52:56 +0100

    MC: filenames without spaces

--- RunMonteCarlo/RunMonteCarlo.cpp
+++ RunMonteCarlo/RunMonteCarlo.cpp
@@ -6,6 +6,14 @@ const double he3_eta_threshold=1.5727;
 const double beam_low=1.426;
 const double beam_hi=1.635;
 using namespace std;
+string ReplaceAll(string str, const string& from, const string& to) {
+	size_t start_pos = 0;
+	while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length();
+	}
+	return str;
+}
 int main(int, char **){
 	PBeamSmearing *smear = new PBeamSmearing("beam_smear", "Beam smearing");
 	smear->SetReaction("p+d");
@@ -15,9 +23,9 @@ int main(int, char **){
 	reactlist.push_back("He3 eta");
 	reactlist.push_back("He3 eta [g g]");
 	reactlist.push_back("He3 eta [pi0 pi0 pi0]");
-	for(string react:reactlist){
+	for(auto react:reactlist){
 		PReaction my_reaction(beam_hi,"p","d",
-			const_cast<char*>(react.c_str()), const_cast<char*>((react).c_str())
+			const_cast<char*>(react.c_str()), const_cast<char*>(ReplaceAll(react," ","-").c_str())
 		,1,0,0,0);
 		my_reaction.Loop(1000000);
 	}
