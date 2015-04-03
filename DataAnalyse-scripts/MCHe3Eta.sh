9e03b5a88703a568a1837e844840ecb8283428c3
rundel@wasa00 <kernphysiker@openmailbox.org>
2015-04-03 12:55:53 +0200

    run script fix

--- DataAnalyse-scripts/MCHe3Eta.sh
+++ DataAnalyse-scripts/MCHe3Eta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N ANALYSE_WMC
 #PBS -l walltime=12:00:00
-cd ../DataAnalyse
+cd ~/WASA-analysis/DataAnalyse
 ./main MCHe3Eta Raw -mode mc -fin file:$WMC_DATA/etap.ems -n MCHe3Eta -abort 
 cd ../DataAnalyse-scripts
