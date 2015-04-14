0b4f5769d6f21f59ce0416152c88571b726991f9
alexkernphysiker <kernphysiker@openmailbox.org>
2015-04-15 01:04:14 +0200

    script update

--- DataAnalyse-scripts/analyse_runs.sh
+++ DataAnalyse-scripts/analyse_runs.sh
@@ -2,11 +2,15 @@ indir="/data0/AllRuns"
 for X in ${RUNS_SEQ}
   do
      scriptname="run_${X}.sh"
-     echo "#!/bin/bash" >>${scriptname}
-     echo >>${scriptname}
-     echo "cd $PWD/../DataAnalyse" >>${scriptname}
+     rm -f ${scriptname}
+     echo "#!/bin/bash" >> ${scriptname}
+     echo "#PBS -N ANALYSE_DATA_${X}" >> ${scriptname}
+     echo "#PBS -l walltime=48:00:00" >> ${scriptname}
+
+     echo >> ${scriptname}
+     echo "cd $PWD/../DataAnalyse" >> ${scriptname}
      echo " ./main $1 -fin cluster:${indir}/run_${X} -r ${X} -n $1_run_${X} -lf run_${X}.log -abort " >> ${scriptname}
-     echo >>${scriptname}
+     echo >> ${scriptname}
      echo "rm -f ${scriptname}" >> ${scriptname}
      chmod u+x ${scriptname}
      qsub ${scriptname}
