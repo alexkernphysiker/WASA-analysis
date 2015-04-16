c5544dba504f2b373d0b497ebcc8fe8477b9bc65
alexkernphysiker <kernphysiker@openmailbox.org>
2015-04-16 12:37:46 +0200

    scripts update

--- DataAnalyse-scripts/analyse_runs.sh
+++ DataAnalyse-scripts/analyse_runs.sh
@@ -1,4 +1,3 @@
-indir="/data0/AllRuns"
 for X in ${RUNS_SEQ}
   do
      scriptname="run_${X}.sh"
@@ -9,7 +8,7 @@ for X in ${RUNS_SEQ}
 
      echo >> ${scriptname}
      echo "cd $PWD/../DataAnalyse" >> ${scriptname}
-     echo " ./main $1 -fin cluster:${indir}/run_${X} -r ${X} -n $1_run_${X} -lf run_${X}.log -abort " >> ${scriptname}
+     echo "./main Data$1 -fin cluster:${RUNS_DATA}/run_${X} -r ${X} -n Data$1_run_${X} -lf run_${X}.log -abort " >> ${scriptname}
      echo >> ${scriptname}
      echo "rm -f ${scriptname}" >> ${scriptname}
      chmod u+x ${scriptname}
@@ -17,3 +16,16 @@ for X in ${RUNS_SEQ}
      echo "${scriptname} generated and executed"
      sleep 2 
 done
+scriptname="run_mc.sh"
+rm -f ${scriptname}
+echo "#!/bin/bash" >> ${scriptname}
+echo "#PBS -N ANALYSE_DATA_${X}" >> ${scriptname}
+echo "#PBS -l walltime=48:00:00" >> ${scriptname}
+echo >> ${scriptname}
+echo "cd $PWD/../DataAnalyse" >> ${scriptname}
+echo "./main MC$1 -mode mc -fin file:$WMC_DATA/etap.ems -n MC$1 -abort" >> ${scriptname}
+echo >> ${scriptname}
+echo "rm -f ${scriptname}" >> ${scriptname}
+chmod u+x ${scriptname}
+qsub ${scriptname}
+echo "${scriptname} generated and executed"
