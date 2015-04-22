4635596447eaaacdde4e6cec3251c04e561ea53e
rundel@wasa00 <kernphysiker@openmailbox.org>
2015-04-22 16:21:33 +0200

    configuration scripts

---
+++ DataAnalyse-scripts/config/config.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+cp FDEdep2Ekin_3He $ROOTSORTERSYS/wasa/Database
+cp Calibration/* $ROOTSORTERSYS/wasa/Database/PreviousRuns/
+mkdir $WASA_ROOT/alig/m4/B009
+cp WMC_Geometry/* $WASA_ROOT/alig/m4/B009
+cp al4cosy0_mc_009.dat042.m4 $WASA_ROOT/alig/m4
+cd $WASA_ROOT/alig/m4 
+./mkalig.sh
+
