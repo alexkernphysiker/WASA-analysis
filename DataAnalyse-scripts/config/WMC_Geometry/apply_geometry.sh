8f0de1ea2965c1a5a9112c7115843e58d0597566
rundel@wasa00 <kernphysiker@openmailbox.org>
2015-05-11 12:27:20 +0200

    Configure scripts fix

--- DataAnalyse-scripts/config/WMC_Geometry/apply_geometry.sh
+++ DataAnalyse-scripts/config/WMC_Geometry/apply_geometry.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 mkdir $WASA_ROOT/alig/m4/B009
-cp WMC_Geometry/* $WASA_ROOT/alig/m4/B009
+cp B009/* $WASA_ROOT/alig/m4/B009
 cp al4cosy0_mc_009.dat042.m4 $WASA_ROOT/alig/m4
 cp mkalig.sh $WASA_ROOT/alig/m4
 (cd $WASA_ROOT/alig/m4;./mkalig.sh)
--- DataAnalyse-scripts/config/config.sh
+++ DataAnalyse-scripts/config/config.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 (cd Reconstruction; ./add_reconstruction_table.sh)
-(cd CalibrationFiles; ./apply_calibration_files.sh)
+(cd Calibration; ./apply_calibration_files.sh)
 (cd WMC_Geometry; ./apply_geometry.sh)
