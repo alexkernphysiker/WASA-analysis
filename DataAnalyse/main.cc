348239a4a957e8b3765ecf0ec64bf0947c1ea619
alexkernphysiker <kernphysiker@openmailbox.org>
2015-05-21 16:43:24 +0200

    Log level = Warning

--- DataAnalyse/main.cc
+++ DataAnalyse/main.cc
@@ -3,7 +3,7 @@
 #include "config.h"
 using namespace std;
 int main(int argc, char** argv) {
-	InitLog(LogDebug,argv[1]);
+	InitLog(LogWarning,argv[1]);
 	int new_c=argc-1;
 	char *args[new_c+1];
 	args[0]=argv[0];
