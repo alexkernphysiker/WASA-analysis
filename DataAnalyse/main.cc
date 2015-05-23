01aa00942665f826dc60d735f7ab42a8a58ff04f
alexkernphysiker <kernphysiker@openmailbox.org>
2015-05-23 16:05:36 +0200

    log level error

--- DataAnalyse/main.cc
+++ DataAnalyse/main.cc
@@ -3,7 +3,7 @@
 #include "config.h"
 using namespace std;
 int main(int argc, char** argv) {
-	InitLog(LogWarning,argv[1]);
+	InitLog(LogError,argv[1]);
 	int new_c=argc-1;
 	char *args[new_c+1];
 	args[0]=argv[0];
