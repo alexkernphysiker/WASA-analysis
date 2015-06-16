829fc5a69d40b677ca3d1644d67cba82953823e7
alexkernphysiker <kernphysiker@openmailbox.org>
2015-06-16 22:45:30 +0200

    WMC scripts refactoring

--- DataAnalyse-scripts/He3Eta_wmc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#PBS -q mc
-#PBS -N WMC
-#PBS -l walltime=2:00:00:00
-cd WMC
-./wmc.sh $PLUTO_OUTPUT/He3Eta_gg_.root $WMC_DATA/He3Eta.wmc.data
--- DataAnalyse-scripts/analyse_runs.sh
+++ DataAnalyse-scripts/analyse_runs.sh
@@ -3,7 +3,7 @@ for X in ${RUNS_SEQ}
      scriptname="run_${X}.sh"
      rm -f ${scriptname}
      echo "#!/bin/bash" >> ${scriptname}
-     echo "#PBS -N ANALYSE_DATA_${X}_$1" >> ${scriptname}
+     echo "#PBS -N DATA_${X}_$1" >> ${scriptname}
      echo "#PBS -l walltime=48:00:00" >> ${scriptname}
 
      echo >> ${scriptname}
@@ -19,7 +19,7 @@ done
 scriptname="run_mc.sh"
 rm -f ${scriptname}
 echo "#!/bin/bash" >> ${scriptname}
-echo "#PBS -N ANALYSE_MC_$1" >> ${scriptname}
+echo "#PBS -N MC__$1" >> ${scriptname}
 echo "#PBS -l walltime=48:00:00" >> ${scriptname}
 echo >> ${scriptname}
 echo "cd $PWD/../DataAnalyse" >> ${scriptname}
@@ -27,5 +27,23 @@ echo "./main MC$1 -mode mc -fin file:$WMC_DATA/$1.wmc.data -n MC$1 -abort" >> ${
 echo >> ${scriptname}
 echo "rm -f ${scriptname}" >> ${scriptname}
 chmod u+x ${scriptname}
-qsub ${scriptname}
+scriptname2="run_mc_prelude.sh"
+rm -f ${scriptname2}
+echo "#!/bin/bash" >> ${scriptname2}
+echo "#PBS -N MC_$1" >> ${scriptname2}
+echo "#PBS -l walltime=48:00:00" >> ${scriptname2}
+echo >> ${scriptname2} 
+echo "FILE = $WMC_DATA/$1.wmc.data" >> ${scriptname2}
+echo "if [-f FILE];" >> ${scriptname2}
+echo "then" >> ${scriptname2}
+echo "./$scriptname" >> ${scriptname2}
+echo "else" >> ${scriptname2}
+echo "cd $PWD/WMC" >> ${scriptname2}
+echo "./wmc.sh $PLUTO_OUTPUT/$1.root $WMC_DATA/$1.wmc.data" >> ${scriptname2}
+echo "qsub $scriptname" >> ${scriptname2}
+echo "fi" >> ${scriptname2}
+echo >> ${scriptname2}
+echo "rm -f ${scriptname2}" >> ${scriptname2}
+chmod u+x ${scriptname2}
+qsub ${scriptname2}
 echo "${scriptname} generated and executed"
