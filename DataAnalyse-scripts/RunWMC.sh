42d753e1564150d6fb1134bba35b973f2a716590
rundel@wasa00 <kernphysiker@openmailbox.org>
2015-06-17 10:03:40 +0200

    WMC script task name fix

--- DataAnalyse-scripts/RunWMC.sh
+++ DataAnalyse-scripts/RunWMC.sh
@@ -1,7 +1,7 @@
 scriptname="run_wmc.sh"
 rm -f ${scriptname}
 echo "#!/bin/bash" >> ${scriptname}
-echo "#PBS -N MC_$1" >> ${scriptname}
+echo "#PBS -N WMC_$1" >> ${scriptname}
 echo "#PBS -l walltime=48:00:00" >> ${scriptname}
 echo >> ${scriptname}
 echo "cd $PWD/WMC" >> ${scriptname}
