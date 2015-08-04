bc29dc7aa13a657c88d2924853170a0d314376b0
alexkernphysiker <kernphysiker@openmailbox.org>
2015-08-04 16:06:50 +0200

    files for reconstruction fit application added

---
+++ Reconstruction/fitpolynom.cpp
@@ -0,0 +1,17 @@
+// this file is distributed under 
+// GPL v 3.0 license
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <memory>
+#include <unistd.h>
+#include <fit.h>
+#include <paramfunc.h>
+#include <filter.h>
+#include <initialconditions.h>
+#include <phys_constants.h>
+using namespace std;
+using namespace Genetic;
+int main(int,char**){
+	auto points=make_shared<FitPoints>();
+}
