9bc9be83db81762aca0e98b6fd064dc0926103ae
alexkernphysiker <kernphysiker@openmailbox.org>
2015-08-05 12:10:45 +0200

    Scripts update

---
+++ DataAnalyse-scripts/RunPreanalysis-mc-only.sh
@@ -0,0 +1,13 @@
+scriptname="run_mc.sh"
+rm -f ${scriptname}
+echo "#!/bin/bash" >> ${scriptname}
+echo "#PBS -N PRE_MC_$1" >> ${scriptname}
+echo "#PBS -l walltime=48:00:00" >> ${scriptname}
+echo >> ${scriptname}
+echo "cd $PWD/../DataAnalyse" >> ${scriptname}
+echo "./main MC_$1 -mode mc -fin file:$WMC_DATA/$1.wmc.data -n PRE_MC$1 -abort" >> ${scriptname}
+echo >> ${scriptname}
+echo "rm -f $PWD/${scriptname}" >> ${scriptname}
+chmod u+x ${scriptname}
+qsub ${scriptname}
+echo "${scriptname} generated and executed"
