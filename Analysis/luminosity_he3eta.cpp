44c77edd26a437c42f6c1eff5f19adbdbb4c6272
alexkernphysiker <kernphysiker@openmailbox.org>
2015-08-07 17:40:45 +0200

    beginning of histogram fitting

--- Analysis/luminosity_he3eta.cpp
+++ Analysis/luminosity_he3eta.cpp
@@ -13,15 +13,18 @@
 using namespace std;
 using namespace Genetic;
 const size_t bg_power=2;
+Hist2Hist::bg_func bg_polynom=[](double x,ParamSet&&P){return Polynom(x,P,bg_power,1);};
 int main(int,char**){
 #include "env.cc"
 	Plotter::Instance().SetOutput(outpath,"he3eta");
 	vector<string> kin_path={"Histograms","Kinematics"};
 	auto missingmass_mc=make_shared<hist>(false,"He3eta_gg_",static_right(kin_path),"MissingMass1633");
 	auto missingmass_data=make_shared<hist>(true,"He3eta_gg_",static_right(kin_path),"MissingMass1633");
-	FitHist<DifferentialMutations<>> fit(missingmass_data,missingmass_mc,[](double x,ParamSet&&P){
-		return Polynom(x,P,bg_power,1);
-	});
+	FitHist<DifferentialMutations<>> fit(missingmass_data,missingmass_mc,bg_polynom);
+	auto init=make_shared<GenerateByGauss>()<<make_pair(1,1);
+	for(size_t i=0;i<=bg_power;i++)init<<make_pair(0,1);
+	fit.Init((bg_power+2)*20,init);
+	
 	PlotHist().Hist("MC",missingmass_mc);
 	PlotHist().Hist("Data",missingmass_data);
 }
