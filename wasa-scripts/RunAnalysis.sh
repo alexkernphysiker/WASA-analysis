4ffc18f342122f2654349c67d8ea031ecd8b0d3c
alexkernphysiker <kernphysiker@openmailbox.org>
2015-08-12 12:54:30 +0200

    time for queue exceeded the range

--- wasa-scripts/RunAnalysis.sh
+++ wasa-scripts/RunAnalysis.sh
@@ -4,7 +4,7 @@ for X in ${RUNS_SEQ}
      rm -f ${scriptname}
      echo "#!/bin/bash" >> ${scriptname}
      echo "#PBS -N DATA_${X}_$1" >> ${scriptname}
-     echo "#PBS -l walltime=72:00:00" >> ${scriptname}
+     echo "#PBS -l walltime=48:00:00" >> ${scriptname}
 
      echo >> ${scriptname}
      echo "cd $PWD/../Preselection" >> ${scriptname}
