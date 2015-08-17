af6ced5e3dc94da16c0b950e242021298fadacff
alexkernphysiker <kernphysiker@openmailbox.org>
2015-08-17 18:11:42 +0200

    scripts update

--- wasa-scripts/RunAnalysis-data.sh
+++ wasa-scripts/RunAnalysis-data.sh
@@ -36,27 +36,4 @@ for X in `seq 45873 1 46884`
 	echo "no file ${RUNS_DATA}/run_${X}"
 	fi
 done
-if [ -f $WMC_DATA/$1.wmc.data ]
-then
-	if [ -f $PWD/../Preselection/MC$1.root ]
-	then
-echo "MC$1.root already exists"
-	else
-scriptname="run_mc.sh"
-rm -f ${scriptname}
-echo "#!/bin/bash" >> ${scriptname}
-echo "#PBS -N MC_$1" >> ${scriptname}
-echo "#PBS -l walltime=48:00:00" >> ${scriptname}
-echo >> ${scriptname}
-echo "cd $PWD/../Preselection" >> ${scriptname}
-echo "./main MC_$1 -mode mc -fin file:$WMC_DATA/$1.wmc.data -n MC$1 -abort" >> ${scriptname}
-echo >> ${scriptname}
-echo "rm -f $PWD/${scriptname}" >> ${scriptname}
-chmod u+x ${scriptname}
-qsub ${scriptname}
-echo "${scriptname} generated and executed"
-	fi
-else
-echo "no file $WMC_DATA/$1.wmc.data"
-fi
 
