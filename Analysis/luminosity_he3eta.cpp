80cf0dd74e8d2f5f3d0369cae1fb2809b4139a68
alexkernphysiker <kernphysiker@openmailbox.org>
2015-08-29 00:45:42 +0200

    Luminosity calculations fix

--- Analysis/luminosity_he3eta.cpp
+++ Analysis/luminosity_he3eta.cpp
@@ -24,31 +24,32 @@ void AnalyseMMSpectra(hist::point&BeamMomentaBin,hist&data,vector<hist>&MC){
 int main(int,char**){
 #include "env.cc"
 	Plotter::Instance().SetOutput(outpath,"he3eta");
-	vector<string> kin_path={"Histograms","Kinematics"};
-	vector<string> norm_path={"Histograms","EventsCount"};
+	vector<string> kin_path={"Histograms","MissingMass"};
+	vector<string> norm_path={"Histograms","FRH1"};
 	
-	hist mc_norm(false,"He3eta",static_right(norm_path),"AllEventsOnPBeam"),
-		mc_filtered(false,"He3eta",static_right(norm_path),"FilteredEventsOnPBeam"),
-		acceptance(false,"He3eta",static_right(norm_path),"ReconstructedEventsOnPBeam");
+	hist mc_norm(false,"He3eta",static_right(norm_path),"Reference"),
+		mc_filtered1(false,"He3eta",static_right(norm_path),"SP2cut"),
+		mc_filtered2(false,"He3eta",static_right(norm_path),"ThetaCut"),
+		acceptance(false,"He3eta",static_right(norm_path),"Reconstructed");
 	PlotHist().Hist("All MC events",static_right(mc_norm))
-		.Hist("Preselected",static_right(mc_filtered))
+		.Hist("EdepCut",static_right(mc_filtered1))
+		.Hist("ThetaCut",static_right(mc_filtered2))
 		.Hist("Reconstructed",static_right(acceptance));
 	acceptance/=mc_norm;
 	PlotHist().Hist("Acceptance",static_right(acceptance));
-	string missingmass="MissingMass";
 	hist luminocity=acceptance.CloneEmptyBins();
 	for(auto&BeamMomentaBin:luminocity){
 		string suffix=to_string(int(BeamMomentaBin.x*1000));
 		vector<hist> MC={
-			hist(false,"He3eta",static_right(kin_path),missingmass+suffix),
-			hist(false,"He3pi0pi0",static_right(kin_path),missingmass+suffix),
-			hist(false,"He3pi0pi0pi0",static_right(kin_path),missingmass+suffix)
+			hist(false,"He3eta",static_right(kin_path),suffix),
+			hist(false,"He3pi0pi0",static_right(kin_path),suffix),
+			hist(false,"He3pi0pi0pi0",static_right(kin_path),suffix)
 		};
 		PlotHist()
 			.Hist(string("MCHe3eta")+suffix,static_cast<hist&&>(MC[0]))
 			.Hist(string("MCHe3 2pi0")+suffix,static_cast<hist&&>(MC[1]))
 			.Hist(string("MCHe3 3pi0")+suffix,static_cast<hist&&>(MC[2]));
-		hist data(true,"He3eta",static_right(kin_path),missingmass+suffix);
+		hist data(true,"He3eta",static_right(kin_path),suffix);
 		PlotHist().Hist(string("DataHe3eta")+suffix,static_right(data));
 		AnalyseMMSpectra(BeamMomentaBin,data,MC);
 	}
