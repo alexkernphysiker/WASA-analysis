e7249a1f5697c8fc9cd137375d092ffa845c64c1
alexkernphysiker <kernphysiker@openmailbox.org>
2015-08-29 13:56:37 +0200

    Data analysing script update

--- wasa-scripts/RunAnalysis-data.sh
+++ wasa-scripts/RunAnalysis-data.sh
@@ -1,39 +1,44 @@
-if [ `qstat|grep $1|wc -l` -gt 0 ]
+if [ `qstat|wc -l` -gt 50 ]
 then
-	echo "The task queue allready contains tasks for this reaction"
+	echo "Your task queue contains too much tasks"
 	exit 1
 fi
 for X in `seq 45873 1 46884`
   do
+	echo "Analysis for run ${X} ($1 reaction)"
 	if [ -f ${RUNS_DATA}/run_${X} ]
 	then
 		if [ -f $PWD/../Preselection/Data$1_run_${X}.root ]
 		then
-	echo "Data$1_run_${X}.root already exists"
+			echo "Has been analysed."
 		else
 			if [ `qstat|wc -l` -lt 50 ]	
 			then
-     scriptname="run_${X}.sh"
-     rm -f ${scriptname}
-     echo "#!/bin/bash" >> ${scriptname}
-     echo "#PBS -N DATA_${X}_$1" >> ${scriptname}
-     echo "#PBS -l walltime=48:00:00" >> ${scriptname}
-
-     echo >> ${scriptname}
-     echo "cd $PWD/../Preselection" >> ${scriptname}
-     echo "./main Data_$1 -fin cluster:${RUNS_DATA}/run_${X} -r ${X} -n Data$1_run_${X} -lf run_${X}.log -abort " >> ${scriptname}
-     echo >> ${scriptname}
-     echo "rm -f $PWD/${scriptname}" >> ${scriptname}
-     chmod u+x ${scriptname}
-     qsub ${scriptname}
-     echo "${scriptname} generated and executed"
-     sleep 2 
+				if [ `qstat|grep ${X}_$1|wc -l` -gt 0 ]
+				then
+					echo "Is being analysed."
+				else
+					scriptname="run_${X}.sh"
+					rm -f ${scriptname}
+					echo "#!/bin/bash" >> ${scriptname}
+					echo "#PBS -N DATA_${X}_$1" >> ${scriptname}
+					echo "#PBS -l walltime=48:00:00" >> ${scriptname}
+					echo >> ${scriptname}
+					echo "cd $PWD/../Preselection" >> ${scriptname}
+					echo "./main Data_$1 -fin cluster:${RUNS_DATA}/run_${X} -r ${X} -n Data$1_run_${X} -lf run_${X}.log -abort " >> ${scriptname}
+					echo >> ${scriptname}
+					echo "rm -f $PWD/${scriptname}" >> ${scriptname}
+					chmod u+x ${scriptname}
+					qsub ${scriptname}
+					echo "HAS BEEN STARTED."
+					sleep 2
+				fi 
 			else
-			echo "Too many jobs"
+				echo "Has not been started (too many jobs are running)"
 			fi
 		fi
 	else
-	echo "no file ${RUNS_DATA}/run_${X}"
+		echo "Has not been started (no such run)"
 	fi
 done
 
