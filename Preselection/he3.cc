220b1e5f30263f84e283825a8443b0123a6b061f
alexkernphysiker <kernphysiker@openmailbox.org>
2015-09-09 22:08:48 +0200

    Using condition given by Nils for theta reconstruction

--- Preselection/he3.cc
+++ Preselection/he3.cc
@@ -51,8 +51,11 @@ He3_in_forward::He3_in_forward():Analysis(),ForwardDetectors(2),
 	});
 	Cut.AddCondition("Edep_cuts",[this](WTrack&track,vector<double>&P){
 		return CutFRH1.Check(track,P)||CutFTH1.Check(track,P);
-	}).AddCondition("ThetaCut",[this](WTrack&track,vector<double>&){
-		return ((track.Theta()<0.1245)||(track.Theta()>0.1255));
+	}).AddCondition("ThetaReconstructable",[this](WTrack&track,vector<double>&){
+		return track.IsInTrack(16) || track.IsInTrack(17) 
+		|| track.IsInTrack(18) || track.IsInTrack(19) 
+		|| track.IsInTrack(20) || track.IsInTrack(21) 
+		|| track.IsInTrack(22) || track.IsInTrack(23);
 	}).AddParameter("E",[this](WTrack&track){
 		return He3_Ekin.Reconstruct(track);
 	}).AddParameter("Theta",[this](WTrack&track){
