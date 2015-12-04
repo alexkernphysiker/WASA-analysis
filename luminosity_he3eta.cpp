46be42520fc9619489d96eb7050ee275f1631b07
alexkernphysiker <kernphysiker@openmailbox.org>
2015-12-04 14:59:23 +0100

    Fitting data as sum of gauss and polynom

--- luminosity_he3eta.cpp
+++ luminosity_he3eta.cpp
@@ -16,8 +16,8 @@
 using namespace std;
 using namespace Genetic;
 RANDOM engine;
-#define Q_range 18.0,28.0
-#define MissingMass_range 0.4,0.6
+#define Q_range 18.0,30.0
+#define MissingMass_range 0.5,0.6
 int main(int,char**){
 	Plotter::Instance().SetOutput(ENV(OUTPUT_PLOTS),"he3eta");
 	hist mc_norm(MC,"He3eta",{"Histograms","Reconstruction"},"Reference");
@@ -33,6 +33,7 @@ int main(int,char**){
 	(acceptance/=mc_norm).Cut(Q_range);
 	PlotHist().Hist("Acceptance",acceptance)<<"set xlabel 'Q, MeV'"<<"set ylabel 'Acceptance, n.d.'"<<"set nokey";
 	hist luminosity=acceptance.CloneEmptyBins();
+	LinearInterpolation<double> ChiSqMCPeak,ChiSqData;
 	for(auto&qBin:luminosity){
 		int index=int(qBin.x*binning_coefficient);
 		hist
@@ -40,10 +41,6 @@ int main(int,char**){
 			MC_He3eta(MC,"He3eta",{"Histograms","MissingMass"},to_string(index)),
 			MC_He3pi0pi0(MC,"He3pi0pi0",{"Histograms","MissingMass"},to_string(index)),
 			MC_He3pi0pi0pi0(MC,"He3pi0pi0pi0",{"Histograms","MissingMass"},to_string(index));
-		DATAhist.Cut(MissingMass_range);
-		MC_He3eta.Cut(MissingMass_range);
-		MC_He3pi0pi0.Cut(MissingMass_range);
-		MC_He3pi0pi0pi0.Cut(MissingMass_range);
 		string suffix=string("Q=")+to_string(qBin.x)+"MeV";
 		PlotHist()
 			.HistWLine(string("MCHe3eta")+suffix,MC_He3eta)
@@ -58,30 +55,43 @@ int main(int,char**){
 		
 		typedef Mul<Par<0>,Func3<Gaussian,Arg<0>,Par<1>,Par<2>>> Peak;
 		#define init make_pair(m_eta,0.005)<<make_pair(0.0,0.01)<<make_pair(0.0,0.01)
+		#define filter make_shared<Above>()<<0<<0<<0
+		typedef PolynomFunc<0,3,6> BG;
 		
-		FitFunction<DifferentialMutations<>,Peak,ChiSquare> fitMC(make_shared<FitPoints>()<<MC_He3eta.Cut(0.54,0.56));
-		fitMC.SetFilter(make_shared<Above>()<<0<<0<<0).SetThreadCount(thr)
+		FitFunction<DifferentialMutations<>,Peak,ChiSquare> 
+			fitMC(make_shared<FitPoints>()<<MC_He3eta.Cut(0.54,0.56));
+		fitMC.SetFilter(filter).SetThreadCount(thr)
 			.Init(pop_size,make_shared<GenerateByGauss>()<<make_pair(200,300)<<init,engine);
-		while(!fitMC.AbsoluteOptimalityExitCondition(accu))
+		while(!fitMC.AbsoluteOptimalityExitCondition(accu)){
 			fitMC.Iterate(engine);
+			cout<<"Peak: "<<fitMC.Optimality()
+				<<"<s<"<<fitMC.Optimality(fitMC.PopulationSize()-1)<<"    \r";
+		}
+		cout<<endl;
 		PlotFit1D<decltype(fitMC)>()
 			.Fit("mc_fit_"+suffix,"mc_"+suffix,fitMC,plot_step)
 			<<"set xlabel 'MM, GeV'"<<"set ylabel 'counts'";
+		ChiSqMCPeak<<make_pair(qBin.x,fitMC.Optimality());
 
-		Fit<DifferentialMutations<>,ChiSquare> fitdata(
-			make_shared<FitPoints>()<<DATAhist,
-			[&MC_He3pi0pi0,&MC_He3pi0pi0pi0](const ParamSet&X,const ParamSet&P){
-				static Peak peak;
-				return peak(X,P)+P[3]*MC_He3pi0pi0(X[0]).y+P[4]*MC_He3pi0pi0pi0(X[0]).y;
-			}
-		);
-		fitdata.SetFilter(make_shared<Above>()<<0<<0<<0).SetThreadCount(thr)
-			.Init(pop_size,make_shared<GenerateByGauss>()<<make_pair(1,2)<<init<<make_pair(1,1)<<make_pair(1,1),engine);
-		while(!fitdata.AbsoluteOptimalityExitCondition(accu))
+		FitFunction<DifferentialMutations<>,Add<Peak,BG>,ChiSquare> 
+			fitdata(make_shared<FitPoints>()<<DATAhist.Cut(MissingMass_range));
+		fitdata.SetFilter(filter).SetThreadCount(thr);
+		auto Init=make_shared<GenerateByGauss>()<<make_pair(1,2)<<init;
+		Init<<make_pair(1000,1000)<<make_pair(0,1000)<<make_pair(0,10);
+		while((Init<<make_pair(0,1))->Count()< Add<Peak,BG>::ParamCount){}
+		fitdata.Init(pop_size,Init,engine);
+		while(!fitdata.AbsoluteOptimalityExitCondition(accu)){
 			fitdata.Iterate(engine);
+			cout<<"Data: "<<fitdata.Optimality()
+			<<"<s<"<<fitdata.Optimality(fitdata.PopulationSize()-1)<<"    \r";
+		}
+		cout<<endl;
 		PlotFit1D<decltype(fitdata)>()
 			.Fit("data_fit_"+suffix,"data_"+suffix,fitdata,plot_step)
+			.ParamFunc("Peak_"+suffix,Peak(),fitdata,plot_step)
+			.ParamFunc("BG_"+suffix,BG(),fitdata,plot_step)
 			<<"set xlabel 'MM, GeV'"<<"set ylabel 'counts'";
+		ChiSqData<<make_pair(qBin.x,fitdata.Optimality());
 		
 		double yd=fitdata[0],ym=fitMC[0];
 		cout <<"Y = "<<yd<<" ; "<<ym<<endl;
@@ -90,10 +100,11 @@ int main(int,char**){
 		qBin.y=double(MC_events_count)*yd/ym;
 		qBin.dy=double(MC_events_count)*sqrt(pow(dd/ym,2)+pow(dm*yd/pow(ym,2),2));
 	}
+	PlotPoints<double,decltype(ChiSqData)>()
+		.LineOnly("Chi^2_data",ChiSqData).LineOnly("Chi^2_MC",ChiSqMCPeak);
 	PlotHist().Hist("He3eta true events in data",luminosity)
 		<<"set xlabel 'Q, MeV'"<<"set ylabel 'True events, counts'"<<"set nokey";
-	PlotHist lumplot;
-	lumplot.Hist("analysed",luminosity/=sigmaHe3eta);
-	lumplot.Hist("estimated",luminosity/=PresentRunsAmountRatio("He3"));
-	lumplot<<"set xlabel 'Q, MeV'"<<"set ylabel 'Integrated luminocity, 1/nb'";
+	PlotHist().Hist("analysed",luminosity/=sigmaHe3eta)
+		.Hist("estimated",luminosity/=PresentRunsAmountRatio("He3"))
+		<<"set xlabel 'Q, MeV'"<<"set ylabel 'Integrated luminocity, 1/nb'";
 }
