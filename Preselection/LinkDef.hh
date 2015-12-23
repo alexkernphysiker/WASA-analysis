752100370a3cf46b821cd42c179adf5afea3eb43
alexkernphysiker <kernphysiker@openmailbox.org>
2015-12-23 14:48:31 +0100

    analysis modules fix

--- Preselection/LinkDef.hh
+++ Preselection/LinkDef.hh
@@ -2,6 +2,7 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
+#pragma link C++ class AbstractAnalysis;
 #pragma link C++ class AnalysisModule;
 #pragma link C++ class ReconstructionModule;
 #endif
