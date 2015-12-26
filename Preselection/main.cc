e0b9a9b702526831f0889ae01b85e5b69921bd1d
alexkernphysiker <kernphysiker@openmailbox.org>
2015-12-26 12:56:19 +0200

    small changes

--- Preselection/main.cc
+++ Preselection/main.cc
@@ -16,20 +16,25 @@ int main(int argc, char** argv) {
 		args[i]=argv[i+2];
 	gSorterConfig->ReadCmdLine(new_c,args);
 	if("rec"==mode){
-		if("MC_He3eta"==type)SetAnalysis([](){return new MC_He3eta();});
+		if("MC_He3eta"==type)
+			SetAnalysis([](){return new RE_He3eta();});
 		if(
 			("MC_He3pi0"==type)||
 			("MC_He3pi0pi0"==type)||
 			("MC_He3pi0pi0pi0"==type)
-		)SetAnalysis([](){return new MC_He3pi0();});
+		)
+			SetAnalysis([](){return new RE_He3pi0();});
 	}else if("ana"==mode){
-		if("MC_He3eta"==type)SetAnalysis([](){return new MC_He3eta();});
+		if("MC_He3eta"==type)
+			SetAnalysis([](){return new MC_He3eta();});
 		if(
 			("MC_He3pi0"==type)||
 			("MC_He3pi0pi0"==type)||
 			("MC_He3pi0pi0pi0"==type)
-		)SetAnalysis([](){return new MC_He3pi0();});
-		if("Data_He3"==type)SetAnalysis([](){return new Data_He3();});
+		)
+			SetAnalysis([](){return new MC_He3pi0();});
+		if("Data_He3"==type)
+			SetAnalysis([](){return new Data_He3();});
 	}else{
 		throw math_h_error<IAnalysis>("Unknown analysis mode");
 	}
