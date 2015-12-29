c4a03f0d3f1b44c616c432542f2ade53ea5e0098
alexkernphysiker <kernphysiker@openmailbox.org>
2015-12-29 15:05:55 +0200

    points weighting. fix

--- lib/Reconstruction/reconstruction_fit.cpp
+++ lib/Reconstruction/reconstruction_fit.cpp
@@ -17,14 +17,15 @@ shared_ptr<FitPoints> WeightPoints(
 	size_t index
 ){
 	auto result=make_shared<FitPoints>();
+	cout<<index<<endl<<pos<<endl<<delta<<endl;
 	if(pos.size()!=index)
-		throw math_h_error<FitPoints>("weighting error");
+		throw math_h_error<FitPoints>("weighting error 1");
 	if(delta.size()!=index)
-		throw math_h_error<FitPoints>("weighting error");
+		throw math_h_error<FitPoints>("weighting error 2");
 	if(index>src->dimensions())
-		throw math_h_error<FitPoints>("weighting error");
+		throw math_h_error<FitPoints>("weighting error 3");
 	if(index==src->dimensions()){
-		double beg=src->Ymin(),end=src->Ymax(),d=(end-beg)/50.0;
+		double beg=src->Ymin(),end=src->Ymax(),d=(end-beg)/25.0;
 		for(double y=beg;y<=end;y+=d){
 			double weight=0;
 			for(Point&P:(*src)){
@@ -35,12 +36,12 @@ shared_ptr<FitPoints> WeightPoints(
 			result<<Point(pos,y,weight);
 		}
 	}else{
-		double beg=src->min()[index],end=src->max()[index],d=(end-beg)/50.0;
+		double beg=src->min()[index],end=src->max()[index],d=(end-beg)/25.0;
 		ParamSet new_pos=pos,new_delta=delta;
-		new_pos<<0;delta<<d;
+		new_pos<<0;new_delta<<d;
 		for(double x=beg;x<=end;x+=d){
 			new_pos[index]=x;
-			result<<WeightPoints(src,static_cast<ParamSet&&>(pos),static_cast<ParamSet&&>(delta),index+1);
+			result<<WeightPoints(src,static_cast<ParamSet&&>(new_pos),static_cast<ParamSet&&>(new_delta),index+1);
 		}
 	}
 	return result;
