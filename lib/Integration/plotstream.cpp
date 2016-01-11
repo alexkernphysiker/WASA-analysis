2a1302a2de950c4a20f3873ea5554c0d6852fa91
alexkernphysiker <kernphysiker@openmailbox.org>
2016-01-11 11:54:15 +0100

    headers of two classes

--- lib/Integration/plotstream.cpp
+++ lib/Integration/plotstream.cpp
@@ -1,3 +1,5 @@
 // this file is distributed under 
 // MIT license
+#include <gnuplot_wrap.h>
 #include "plotstream.h"
+ParamSet GM(Point&&p){}
--- lib/Integration/plotstream.h
+++ lib/Integration/plotstream.h
@@ -2,13 +2,34 @@
 // MIT license
 #ifndef IPZWCLTH
 # define IPZWCLTH
-#include <gnuplot_wrap.h>
+#include <string>
+#include <utility>
 #include <Genetic/fit.h>
 namespace PlotStream{
 	using namespace std;
 	using namespace Genetic;
-	
-	class AbstractStream{
+	class AbstractPlotStream{
+	protected:
+		AbstractPlotStream(string&&name);
+	public:
+		AbstractPlotStream&operator<<(const ParamSet&P);
+		AbstractPlotStream&operator<<(ParamSet&&P);
+		AbstractPlotStream&operator<<(const Point&P);
+		AbstractPlotStream&operator<<(Point&&P);
+	protected:
+		virtual void ProcessPoint(const ParamSet&P)=0;
+		string&Name()const;
+	private:
+		string m_name;
+	};
+	class SimpleStream:public AbstractPlotStream{
+	public:
+		SimpleStream(string&& name,pair<size_t,size_t>&&indexes);
+		virtual ~SimpleStream();
+	protected:
+		virtual void ProcessPoint(const ParamSet& P)override;
+	private:
+		vector<pair<double,double>> m_data;
 	};
 };
 #endif
