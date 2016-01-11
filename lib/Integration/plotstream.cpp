f8d6571074cbd1ecda6b9cfcb7ac0f31983c0d89
alexkernphysiker <kernphysiker@openmailbox.org>
2016-01-11 17:06:31 +0100

    first version of plot streams

--- lib/Integration/plotstream.cpp
+++ lib/Integration/plotstream.cpp
@@ -1,5 +1,106 @@
 // this file is distributed under 
 // MIT license
-#include <gnuplot_wrap.h>
+#include <math_h/exception_math_h.h>
 #include "plotstream.h"
-ParamSet GM(Point&&p){}
+namespace PlotStream {
+	using namespace std;
+	using namespace Genetic;
+	ParamSet GM(const Point&p){
+		ParamSet res=p.X();
+		res<<p.y();
+		return res;
+	}
+AbstractPlotStream::AbstractPlotStream(string&& name, shared_ptr<PlotEngine> plot){
+	m_plot=plot;
+	m_name=name;
+}
+AbstractPlotStream::AbstractPlotStream(string&& name):
+	AbstractPlotStream(static_cast<string&&>(name),make_shared<PlotEngine>()){}
+string&&AbstractPlotStream::Name() const{return const_cast<string&&>(m_name);}
+shared_ptr< PlotEngine > AbstractPlotStream::Plot(){return m_plot;}
+AbstractPlotStream::~AbstractPlotStream(){}
+AbstractPlotStream& AbstractPlotStream::operator<<(const ParamSet& P){
+	ProcessPoint(P);
+	return *this;
+}
+AbstractPlotStream& AbstractPlotStream::operator<<(ParamSet&& P){return operator<<(P);}
+AbstractPlotStream& AbstractPlotStream::operator<<(const Point& P){return operator<<(GM(P));}
+AbstractPlotStream& AbstractPlotStream::operator<<(Point&& P){return operator<<(GM(P));}
+SimplePlotStream::SimplePlotStream(string&&name, pair<size_t,size_t>&&indexes,shared_ptr<PlotEngine>plot)
+	:AbstractPlotStream(static_cast<string&&>(name), plot){m_indexes=indexes;}
+
+SimplePlotStream::SimplePlotStream(std::string&& name,pair<size_t,size_t>&& indexes)
+	:AbstractPlotStream(static_cast<string&&>(name)){m_indexes=indexes;}
+SimplePlotStream::~SimplePlotStream(){
+	Plot()->WithoutErrors(Name(),m_data);
+}
+void SimplePlotStream::ProcessPoint(const ParamSet& P){
+	m_data.push_back(make_pair(P[m_indexes.first],P[m_indexes.first]));
+}
+
+Binner::binparam::binparam(size_t ind, double a, double b, size_t cnt)
+	:param_index(ind),from(a),to(b),bin_count(cnt){}
+Binner::binparam::binparam(const Binner::binparam& source)
+	:param_index(source.param_index)
+	,from(source.from),to(source.to)
+	,bin_count(source.bin_count){}
+
+Binner::Binner(string&&name,const Binner::binparam&binning)
+:AbstractPlotStream(static_cast<string&&>(name)),m_binning(binning){
+	CheckCorrectness();
+}
+Binner::Binner(string&& name, Binner::binparam&& binning)
+:AbstractPlotStream(static_cast<string&&>(name)),m_binning(binning){
+	CheckCorrectness();
+}
+void Binner::CheckCorrectness()const{
+	if(m_binning.to<m_binning.from)
+		throw math_h_error<decltype(*this)>("wrong binning ranges");
+	if(0==m_binning.bin_count)
+		throw math_h_error<decltype(*this)>("there cannot be zero bins");
+}
+Binner::~Binner(){}
+size_t Binner::BinCount()const{return m_binning.bin_count;}
+double Binner::BinHW() const{
+	return (m_binning.to-m_binning.from)/double(m_binning.bin_count*2);
+}
+double Binner::BinCenter(size_t i) const{
+	if(i>=m_binning.bin_count)
+		throw math_h_error<decltype(*this)>("bin range check error");
+	return m_binning.from+BinHW()*double(1+2*i);
+}
+bool Binner::FindBinIndex(const ParamSet& P, size_t& res) const{
+	double x=P[m_binning.param_index],delta=BinHW();
+	for(size_t i=0,n=BinCount();i<n;i++)
+		if(
+			(x>=(BinCenter(i)-delta))&&
+			(x<(BinCenter(i)+delta))
+		){
+			res=i;
+			return true;
+		}
+	return false;
+}
+void Binner::Fill(Binner::Creator1 func){
+	m_streams.clear();
+	for(size_t i=0,n=BinCount();i<n;i++)
+		m_streams.push_back(func(i));
+}
+void Binner::Fill(Binner::Creator2 func){
+	m_streams.clear();
+	for(size_t i=0,n=BinCount();i<n;i++)
+		m_streams.push_back(func(i,Name()));
+}
+void Binner::Fill(Binner::Creator3 func){
+	m_streams.clear();
+	for(size_t i=0,n=BinCount();i<n;i++)
+		m_streams.push_back(func(i,Name(),Plot()));
+}
+void Binner::ProcessPoint(const ParamSet& P){
+	if(m_streams.size()==0)
+		throw math_h_error<decltype(*this)>("Attempt to use not filled binner");
+	size_t index=0;
+	if(FindBinIndex(P,index))
+		m_streams[index]->operator<<(P);
+}
+}
--- lib/Integration/plotstream.h
+++ lib/Integration/plotstream.h
@@ -4,32 +4,70 @@
 # define IPZWCLTH
 #include <string>
 #include <utility>
+#include <functional>
 #include <Genetic/fit.h>
+#include <gnuplot_wrap.h>
 namespace PlotStream{
 	using namespace std;
 	using namespace Genetic;
+	typedef PlotPoints<double,vector<pair<double,double>>> PlotEngine;
 	class AbstractPlotStream{
 	protected:
+		AbstractPlotStream(string&&name,shared_ptr<PlotEngine>plot);
 		AbstractPlotStream(string&&name);
+		virtual ~AbstractPlotStream();
 	public:
 		AbstractPlotStream&operator<<(const ParamSet&P);
 		AbstractPlotStream&operator<<(ParamSet&&P);
 		AbstractPlotStream&operator<<(const Point&P);
 		AbstractPlotStream&operator<<(Point&&P);
+		shared_ptr<PlotEngine>Plot();
 	protected:
 		virtual void ProcessPoint(const ParamSet&P)=0;
-		string&Name()const;
+		string&&Name()const;
 	private:
 		string m_name;
+		shared_ptr<PlotEngine> m_plot;
 	};
-	class SimpleStream:public AbstractPlotStream{
+	class SimplePlotStream:public AbstractPlotStream{
 	public:
-		SimpleStream(string&& name,pair<size_t,size_t>&&indexes);
-		virtual ~SimpleStream();
+		SimplePlotStream(string&& name,pair<size_t,size_t>&&indexes);
+		SimplePlotStream(string&& name,pair<size_t,size_t>&&indexes,shared_ptr<PlotEngine> plot);
+		virtual ~SimplePlotStream();
 	protected:
 		virtual void ProcessPoint(const ParamSet& P)override;
 	private:
+		pair<size_t,size_t> m_indexes;
 		vector<pair<double,double>> m_data;
+	};
+	class Binner:public AbstractPlotStream{
+	public:
+		struct binparam{
+			size_t param_index;
+			double from,to;
+			size_t bin_count;
+			binparam(size_t ind,double a,double b,size_t cnt);
+			binparam(const binparam&source);
+		};
+		Binner(string&& name,const binparam& binning);
+		Binner(string&& name,binparam&&binning);
+		virtual ~Binner();
+		typedef function<shared_ptr<AbstractPlotStream>(size_t i)>Creator1;
+		typedef function<shared_ptr<AbstractPlotStream>(size_t i,string&&)>Creator2;
+		typedef function<shared_ptr<AbstractPlotStream>(size_t i,string&&,shared_ptr<PlotEngine>)>Creator3;
+		void Fill(Creator1 func);
+		void Fill(Creator2 func);
+		void Fill(Creator3 func);
+	protected:
+		virtual void ProcessPoint(const ParamSet& P)override;
+		bool FindBinIndex(const ParamSet& P,size_t&res)const;
+		double BinHW()const;
+		double BinCenter(size_t i)const;
+		size_t BinCount()const;
+	private:
+		void CheckCorrectness()const;
+		binparam m_binning;
+		vector<shared_ptr<AbstractPlotStream>> m_streams;
 	};
 };
 #endif
