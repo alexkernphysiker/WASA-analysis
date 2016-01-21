933c6fcbb51cbb15eced43e7ec186dc6aeebd5e6
rundel <alexkernphysiker@openmailbox.org>
2016-01-21 13:56:57 +0100

    fit: parameters count decreased

--- lib/Reconstruction/reconstruction_fit.h
+++ lib/Reconstruction/reconstruction_fit.h
@@ -57,7 +57,7 @@ namespace SimulationDataProcess{
 		});
 		FitFunction<DifferentialMutations<>,FITFUNC,SumWeightedSquareDiff> fit(points);
 		cout<<"Init2"<<endl;
-		fit.SetFilter(filter).Init(30*FITFUNC::ParamCount,init,R);
+		fit.SetFilter(filter).Init(40*FITFUNC::ParamCount,init,R);
 		cout<<"Fitting"<<endl;
 		while(
 			(!fit.AbsoluteOptimalityExitCondition(0.000001))&&
