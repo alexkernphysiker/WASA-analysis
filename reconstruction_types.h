0935d3823ce5064c3bcf7e26510d4ca6d662cb5d
alexkernphysiker <kernphysiker@openmailbox.org>
2016-01-21 10:37:49 +0100

    parameter count decreased

--- reconstruction_types.h
+++ reconstruction_types.h
@@ -5,15 +5,13 @@
 #include <Genetic/paramfunc.h>
 namespace Reconstruction{
 	using namespace Genetic;
-	typedef Add3<
+	typedef Add2<
 		PolynomFunc<1,0,2>,
-		Mul<Arg<0>,PolynomFunc<1,3,2>>,
-		Mul<Sqr<Arg<0>>,PolynomFunc<1,6,2>>
+		Mul<Arg<0>,PolynomFunc<1,3,2>>
 	> He3EnergyFRH1;
-	typedef Add3<
+	typedef Add2<
 		PolynomFunc<1,0,2>,
-		Mul<Arg<0>,PolynomFunc<1,3,2>>,
-		Mul<Sqr<Arg<0>>,PolynomFunc<1,6,2>>
+		Mul<Arg<0>,PolynomFunc<1,3,2>>
 	> He3EnergyFRH2;
 };
 #endif
