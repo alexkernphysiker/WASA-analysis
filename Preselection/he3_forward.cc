277d00a95a48bd9d02d994072e6b736ebe2f756c
alexkernphysiker <kernphysiker@openmailbox.org>
2016-01-28 02:51:29 +0100

    trackprocessing.h refactoring

--- Preselection/he3_forward.cc
+++ Preselection/he3_forward.cc
@@ -46,10 +46,12 @@ namespace ReactionSetup{
 		};
 		return res;
 	}
-	shared_ptr<AbstractChain> FRH1_cut(const Analysis&data){
-		return make_shared<ChainAnd>()
-		<<[](WTrack&T)->bool{return Forward::Get().StoppingLayer(T)==kFRH1;}
-		<<[](WTrack&T)->bool{
+	shared_ptr<ICalculation> FRH1_cut(const Analysis&data){
+		auto res=make_shared<CalcChain>();
+		res<<[](WTrack&T,Results&)->bool{
+			return Forward::Get().StoppingLayer(T)==kFRH1;
+		};
+		res<<[](WTrack&T,Results&)->bool{
 			//Achtung - static
 			static TCutG *cut=nullptr;
 			if(cut==nullptr){
@@ -75,9 +77,9 @@ namespace ReactionSetup{
 			double x=Forward::Get()[kFRH1].Edep(T);
 			double y=Forward::Get()[kFTH1].Edep(T);
 			return cut->IsInside(x,y);
-		}
-		<<Forward::Get().CreateMarker(dirname(),"2.5-FRH1")
-		<<make_shared<Parameter>([&data](WTrack&track)->double{
+		};
+		res<<Forward::Get().CreateMarker(dirname(),"2.5-FRH1");
+		res<<[&data](WTrack&track,Results&P)->bool{
 			//Achtung - static
 			static FitBasedReconstruction<Reconstruction::He3EnergyFRH1,WTrack&> energy(
 				"He3.E.FRH1",
@@ -87,21 +89,25 @@ namespace ReactionSetup{
 				},
 				[&data](WTrack&){return data.FromFirstVertex(kHe3).E;}
 			);
-			return energy.Reconstruct(track);
-		});
+			P.push_back(energy.Reconstruct(track));
+			return true;
+		};
+		return res;
 	}
-	shared_ptr<AbstractChain> FRH2_cut(const Analysis&data){
-		return make_shared<ChainAnd>()
-		<<[](WTrack&T)->bool{return Forward::Get().StoppingLayer(T)==kFRH2;}
-		<<[](WTrack&T)->bool{
+	shared_ptr<ICalculation> FRH2_cut(const Analysis&data){
+		auto res=make_shared<CalcChain>();
+		res<<[](WTrack&T,Results&)->bool{
+			return Forward::Get().StoppingLayer(T)==kFRH2;
+		};
+		res<<[](WTrack&T,Results&)->bool{
 			vector<double> Ed={
 				Forward::Get()[kFRH1].Edep(T),
 				Forward::Get()[kFRH2].Edep(T)
 			};
 			return (Ed[0]>(0.25-0.417*Ed[1]))&&(Ed[0]<(0.35-0.417*Ed[1]))&&(Ed[1]<0.22);
-		}
-		<<Forward::Get().CreateMarker(dirname(),"2.5-FRH2")
-		<<make_shared<Parameter>([&data](WTrack&track)->double{
+		};
+		res<<Forward::Get().CreateMarker(dirname(),"2.5-FRH2");
+		res<<[&data](WTrack&track,Results&P)->bool{
 			//Achtung - static
 			static FitBasedReconstruction<Reconstruction::He3EnergyFRH2,WTrack&> energy(
 				"He3.E.FRH2",
@@ -111,33 +117,58 @@ namespace ReactionSetup{
 				},
 				[&data](WTrack&){return data.FromFirstVertex(kHe3).E;}
 			);
-			return energy.Reconstruct(track);
-		});
+			P.push_back(energy.Reconstruct(track));
+			return true;
+		};
+		return res;
 	}
-	shared_ptr<AbstractChain> ReconstructionProcess(const Analysis&data,const Axis&Q){
-		return make_shared<ChainCheck>()
-		<<Forward::Get().CreateMarker(dirname(),"1-AllTracks")
-		<<make_shared<Hist1D>(dirname(),"1-AllTracks",Q)
-		<<[](WTrack&T)->bool{
+	shared_ptr<ICalculation> ReconstructionProcess(const Analysis&data,const Axis<>&Q){
+		auto res=make_shared<CalcChain>();
+		res<<Forward::Get().CreateMarker(dirname(),"1-AllTracks");
+		res<<make_shared<Hist1D<>>(dirname(),"1-AllTracks",Q);
+			
+		res<<[](WTrack&T,Results&)->bool{
 			//ToDo: replace by more reasonable condition
 			return (T.Theta()!=0.125);
-		}
-		<<Forward::Get().CreateMarker(dirname(),"2-FPC")<<make_shared<Hist1D>(dirname(),"2-FPC",Q)
-		<<(make_shared<ChainOr>()<<FRH1_cut(data)<<FRH2_cut(data))
-		<<Forward::Get().CreateMarker(dirname(),"3-AllCuts")<<make_shared<Hist1D>(dirname(),"3-AllCuts",Q)
-		<<make_shared<Parameter>([](WTrack&T)->double{return T.Theta();})
-		<<make_shared<Parameter>([](WTrack&T)->double{return T.Phi();})
-		<<[](const vector<double>&P)->bool{return isfinite(P[0])&&isfinite(P[1])&&isfinite(P[2]);}
-		<<Forward::Get().CreateMarker(dirname(),"4-Reconstructed")
-		<<make_shared<Hist1D>(dirname(),"4-Reconstructed",Q);
+		};
+			
+		res<<Forward::Get().CreateMarker(dirname(),"2-FPC");
+		res<<make_shared<Hist1D<>>(dirname(),"2-FPC",Q);
+			
+		auto cuts=make_shared<CalcOr>();
+		cuts<<FRH1_cut(data)<<FRH2_cut(data);
+		res<<cuts;
+			
+		res<<Forward::Get().CreateMarker(dirname(),"3-AllCuts");
+		res<<make_shared<Hist1D<>>(dirname(),"3-AllCuts",Q);
+			
+		res<<[](WTrack&T,Results&R)->bool{
+			R.push_back(T.Theta());
+			return true;
+		};
+		res<<[](WTrack&T,Results&R)->bool{
+			R.push_back(T.Phi());
+			return true;
+		};
+		res<<[](WTrack&,Results&R)->bool{
+			return isfinite(R[0])&&isfinite(R[1])&&isfinite(R[2]);
+		};
+			
+		res<<Forward::Get().CreateMarker(dirname(),"4-Reconstructed");
+		res<<make_shared<Hist1D<>>(dirname(),"4-Reconstructed",Q);
+		return res;
 	}
-	shared_ptr<AbstractChain> MissingMass(const Analysis&data,const Axis&Q){
-		Axis M([](const vector<double>&P)->double{return P[3];},m_pi0-0.5,m_eta+0.5,100);
-		return make_shared<Chain>()
-		<<make_shared<Parameter>([&data](const vector<double>&He3)->double{
-			double p=sqrt(He3[0]*(He3[0]+2*m_3He));
+	shared_ptr<ICalculation> MissingMass(const Analysis&data,const Axis<>&Q){
+		Axis<WTrack&,Results&> Q_([Q](WTrack&,Results&){return Q();},Q);
+		Axis<WTrack&,Results&> M([](WTrack&,Results&P)->double{return P[3];},m_pi0-0.5,m_eta+0.5,100);
+		auto res=make_shared<CalcChain>();
+		res<<[&data](WTrack&,Results&P)->bool{
+			double Ekin=P[0];
+			double theta=P[1];
+			double phi=P[2];
+			double p=sqrt(Ekin*(Ekin+2*m_3He));
 			TVector3 p_He3;
-			p_He3.SetMagThetaPhi(p,He3[1],He3[2]);
+			p_He3.SetMagThetaPhi(p,theta,phi);
 			TLorentzVector P_He3;
 			P_He3.SetVectM(p_He3,m_3He);
 			TLorentzVector P_Total;{
@@ -152,48 +183,50 @@ namespace ReactionSetup{
 				P_Total=P_Beam+P_Target;
 			}
 			TLorentzVector P_Missing=P_Total-P_He3;
-			return P_Missing.M();
-		})
-		<<make_shared<SetOfHists1D>(dirname(),"MissingMass",Q,M);
+			P.push_back(P_Missing.M());
+			return true;
+		};
+		res<<make_shared<SetOfHists1D<WTrack&,Results&>>(dirname(),"MissingMass",Q_,M);
+		return res;
 	}
-	shared_ptr<AbstractChain> He3Eta_cut(){
-		return make_shared<ChainCheck>()
-		<<[](WTrack&track)->bool{
+	shared_ptr<ICalculation> He3Eta_cut(){
+		auto res=make_shared<CalcChain>();
+		res<<[](WTrack&track,Results&)->bool{
 			if(Forward::Get().StoppingLayer(track)!=kFRH1)return false;
 			double E=Forward::Get()[kFRH1].Edep(track);
 			return (E>0.08)&&(E<0.22);
 		};
+		return res;
 	}
-	shared_ptr<AbstractChain> KinematicHe3Test(const Analysis&data,He3Modification mode){
-		Axis Bkin([&data]()->double{return 1000.0*Q_He3eta(data.PBeam());},0.0,30.0,12);
-		Axis Ek([](const vector<double>&P)->double{return P[0];},0.1,0.6,50);
-		Axis Ev([&data]()->double{return data.FromFirstVertex(kHe3).E;},0.1,0.6,50);
-		Axis Th([](const vector<double>&P)->double{return P[1];},0.10,0.16,50);
-		Axis Tv([&data]()->double{return data.FromFirstVertex(kHe3).Th;},0.10,0.16,50);
-		auto res=make_shared<Chain>()<<make_shared<SetOfHists2D>(dirname(),"Kinematic-reconstructed",Bkin,Ek,Th);
-		if(mode==forEta)res<<make_shared<SetOfHists2D>(dirname(),"Kinematic-vertex",Bkin,Ev,Tv);
+	shared_ptr<IResAnalysis> KinematicHe3Test(const Analysis&data,He3Modification mode){
+		Axis<const Results&> Bkin([&data](const Results&)->double{return 1000.0*Q_He3eta(data.PBeam());},Bkin);
+		Axis<const Results&> Ev([&data](const Results&)->double{return data.FromFirstVertex(kHe3).E;},0.1,0.6,50);
+		Axis<const Results&> Tv([&data](const Results&)->double{return data.FromFirstVertex(kHe3).Th;},0.10,0.16,50);
+		Axis<const Results&> Ek([](const Results&P)->double{return P[0];},Ev);
+		Axis<const Results&> Th([](const Results&P)->double{return P[1];},Tv);
+		auto res=make_shared<AnalysisSet>();
+		res<<make_shared<SetOfHists2D<const Results&>>(dirname(),"Kinematic-reconstructed",Bkin,Ek,Th);
+		if(mode==forEta)
+			res<<make_shared<SetOfHists2D<const Results&>>(dirname(),"Kinematic-vertex",Bkin,Ev,Tv);
 		return res;
 	}
 	
 	///Reaction analysis types visible from reactions.h
 	Analysis* He3_forward_analyse(He3Modification mode){
 		auto res=Prepare(mode);
-		Axis Q([res]()->double{return 1000.0*Q_He3eta(res->PBeam());},0.0,30.0,12);
-		res->TrackTypeProcess(kFDC)<<(make_shared<ChainCheck>()
-			<<ReconstructionProcess(*res,Q)
-			<<He3Eta_cut()
-			<<KinematicHe3Test(*res,mode)
-			<<MissingMass(*res,Q)
-		);
+		Axis<> Q([res]()->double{return 1000.0*Q_He3eta(res->PBeam());},0.0,30.0,12);
+		auto chain=make_shared<TrackCalcChain>();
+		chain<<ReconstructionProcess(*res,Q)<<He3Eta_cut();
+		chain<<KinematicHe3Test(*res,mode)<<MissingMass(*res,Q);
+		res->TrackTypeProcess(kFDC).Add(chain);
 		return res;
 	}
 	Analysis* He3_forward_reconstruction(He3Modification mode){
 		auto res=Prepare(mode);
-		Axis Q([res]()->double{return 1000.0*Q_He3eta(res->PBeam());},0.0,30.0,12);
-		res->TrackTypeProcess(kFDC)<<(make_shared<ChainCheck>()
-			<<ReconstructionProcess(*res,Q)
-			<<KinematicHe3Test(*res,mode)
-		);
+		Axis<> Q([res]()->double{return 1000.0*Q_He3eta(res->PBeam());},0.0,30.0,12);
+		auto chain=make_shared<TrackCalcChain>();
+		chain<<ReconstructionProcess(*res,Q)<<KinematicHe3Test(*res,mode);
+		res->TrackTypeProcess(kFDC).Add(chain);
 		return res;
 	}
 }
--- Preselection/trackprocessing.cc
+++ Preselection/trackprocessing.cc
@@ -19,6 +19,21 @@
 namespace TrackAnalyse{
 	using namespace std;
 	using namespace MathTemplates;
+	Binner::Binner(double f, double t, unsigned int b){
+		from=f;
+		to=t;
+		bins=b;
+		CheckCorrectness();
+	}
+	Binner::Binner(const Binner& source):Binner(source.from,source.to,source.bins){}
+	Binner& Binner::operator=(const Binner& source){
+		from=source.from;
+		to=source.to;
+		bins=source.bins;
+		CheckCorrectness();
+		return *this;
+	}
+	Binner::~Binner(){}
 	size_t Binner::count() const{return bins;}
 	double Binner::left() const{return from;}
 	double Binner::right() const{return to;}
@@ -53,7 +68,7 @@ namespace TrackAnalyse{
 		gHistoManager->Add(hist,dir.c_str());
 	}
 	AHist1D::~AHist1D(){}
-	void AHist1D::Accept(double x){
+	void AHist1D::Accept(double x)const{
 		hist->Fill(x);
 	}
 	ASetOfHists1D::ASetOfHists1D(const string& dir, const string& name, const Binner& z, const Binner& x):Z(z){
@@ -107,11 +122,21 @@ namespace TrackAnalyse{
 		}
 	}
 
+	class ParamlessConverter:public ICalculation{
+	private:
+		shared_ptr<ParamlessProcess> func;
+	public:
+		ParamlessConverter(shared_ptr<ParamlessProcess> f){func=f;}
+		virtual ~ParamlessConverter(){}
+		virtual bool Process(WTrack&,Results&)const override{
+			return func->Process();
+		}
+	};
 	class AnalysisConverter:public ICalculation{
 	private:
-		shared_ptr<IAnalysis> func;
+		shared_ptr<IResAnalysis> func;
 	public:
-		AnalysisConverter(shared_ptr<IAnalysis> f){func=f;}
+		AnalysisConverter(shared_ptr<IResAnalysis> f){func=f;}
 		virtual ~AnalysisConverter(){}
 		virtual bool Process(WTrack&,Results&P)const override{
 			return func->Process(P);
@@ -127,10 +152,13 @@ namespace TrackAnalyse{
 			return func->Process(T);
 		}
 	};
-	void CalculationSet::Add(shared_ptr<IAnalysis> s){
+	void CalculationSet::AddC(shared_ptr<ParamlessProcess>s){
+		AbstractSet<WTrack&,Results&>::Add(make_shared<ParamlessConverter>(s));
+	}
+	void CalculationSet::AddC(shared_ptr<IResAnalysis> s){
 		AbstractSet<WTrack&,Results&>::Add(make_shared<AnalysisConverter>(s));
 	}
-	void CalculationSet::Add(shared_ptr<ITrackProcess>s){
+	void CalculationSet::AddC(shared_ptr<ITrackProcess>s){
 		AbstractSet<WTrack&,Results&>::Add(make_shared<TrackConverter>(s));
 	}
 	bool TrackCalculations::Process(WTrack& T) const{
