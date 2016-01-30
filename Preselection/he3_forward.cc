28b7c5d02d23dafa94f30ab823e539c481b2c142
alexkernphysiker <kernphysiker@openmailbox.org>
2016-01-30 18:42:37 +0100

    kinematic cuts. stage 2

--- Preselection/he3_forward.cc
+++ Preselection/he3_forward.cc
@@ -160,9 +160,6 @@ namespace ReactionSetup{
 	Axis Phi([](const vector<double>&P)->double{return P[2];},0.0,6.28,360);
 	shared_ptr<ITrackParamProcess> He3Eta_cut(const Analysis&data){
 		return make_shared<ChainCheck>()
-			<<[](WTrack&T,const vector<double>&P)->bool{
-				return(Ek(T,P)>0.248)&&(Ek(T,P)>0.374);
-			}
 			<<(make_shared<ChainBinner>(Axis([&data]()->double{return 1000.0*Q_He3eta(data.PBeam());},0.0,30.0,12))
 				<<[](){return false;}//0
 				<<[](){return false;}
@@ -170,25 +167,124 @@ namespace ReactionSetup{
 				<<[](){return false;}
 				<<[](){return false;}//4
 				<<[](WTrack&T,const vector<double>&P){
-					return (Th(T,P)<0.095);
+					static TCutG *cut=nullptr;
+					if(cut==nullptr){
+						cut=new TCutG("kincut5",5);
+						cut->SetVarX("Ek");
+						cut->SetVarY("Theta");
+						cut->SetPoint(1,0.279,0.095);
+						cut->SetPoint(2,0.279,0.091);
+						cut->SetPoint(3,0.323,0.091);
+						cut->SetPoint(4,0.323,0.095);
+						cut->SetPoint(5,0.279,0.095);
+					}
+					return cut->IsInside(Ek(T,P),Th(T,P));
 				}
 				<<[](WTrack&T,const vector<double>&P){
-					return (Th(T,P)<0.107);
+					static TCutG *cut=nullptr;
+					if(cut==nullptr){
+						cut=new TCutG("kincut6",5);
+						cut->SetVarX("Ek");
+						cut->SetVarY("Theta");
+						cut->SetPoint(1,0.264,0.091);
+						cut->SetPoint(2,0.336,0.091);
+						cut->SetPoint(3,0.310,0.104);
+						cut->SetPoint(4,0.290,0.104);
+						cut->SetPoint(5,0.264,0.091);
+					}
+					return cut->IsInside(Ek(T,P),Th(T,P));
 				}//6
 				<<[](WTrack&T,const vector<double>&P){
-					return (Th(T,P)<0.115);
+					static TCutG *cut=nullptr;
+					if(cut==nullptr){
+						cut=new TCutG("kincut7",7);
+						cut->SetVarX("Ek");
+						cut->SetVarY("Theta");
+						cut->SetPoint(1,0.263,0.091);
+						cut->SetPoint(2,0.342,0.091);
+						cut->SetPoint(3,0.332,0.104);
+						cut->SetPoint(4,0.311,0.112);
+						cut->SetPoint(5,0.286,0.112);
+						cut->SetPoint(6,0.273,0.104);
+						cut->SetPoint(7,0.263,0.091);
+					}
+					return cut->IsInside(Ek(T,P),Th(T,P));
 				}
 				<<[](WTrack&T,const vector<double>&P){
-					return (Th(T,P)<0.117);
+					static TCutG *cut=nullptr;
+					if(cut==nullptr){
+						cut=new TCutG("kincut8",10);
+						cut->SetVarX("Ek");
+						cut->SetVarY("Theta");
+						cut->SetPoint(1 ,0.259,0.091);
+						cut->SetPoint(2 ,0.282,0.091);
+						cut->SetPoint(3 ,0.303,0.098);
+						cut->SetPoint(4 ,0.322,0.091);
+						cut->SetPoint(5 ,0.354,0.091);
+						cut->SetPoint(6 ,0.338,0.107);
+						cut->SetPoint(7 ,0.317,0.115);
+						cut->SetPoint(8 ,0.291,0.115);
+						cut->SetPoint(9 ,0.267,0.107);
+						cut->SetPoint(10,0.259,0.091);
+					}
+					return cut->IsInside(Ek(T,P),Th(T,P));
 				}//8
 				<<[](WTrack&T,const vector<double>&P){
-					return (Th(T,P)<0.122);
+					static TCutG *cut=nullptr;
+					if(cut==nullptr){
+						cut=new TCutG("kincut9",10);
+						cut->SetVarX("Ek");
+						cut->SetVarY("Theta");
+						cut->SetPoint(1 ,0.252,0.091);
+						cut->SetPoint(2 ,0.278,0.091);
+						cut->SetPoint(3 ,0.302,0.101);
+						cut->SetPoint(4 ,0.329,0.091);
+						cut->SetPoint(5 ,0.362,0.091);
+						cut->SetPoint(6 ,0.349,0.110);
+						cut->SetPoint(7 ,0.320,0.120);
+						cut->SetPoint(8 ,0.286,0.120);
+						cut->SetPoint(9 ,0.263,0.110);
+						cut->SetPoint(10,0.252,0.091);
+					}
+					return cut->IsInside(Ek(T,P),Th(T,P));
 				}
 				<<[](WTrack&T,const vector<double>&P){
-					return (Th(T,P)<0.128);
+					static TCutG *cut=nullptr;
+					if(cut==nullptr){
+						cut=new TCutG("kincut10",10);
+						cut->SetVarX("Ek");
+						cut->SetVarY("Theta");
+						cut->SetPoint(1 ,0.249,0.091);
+						cut->SetPoint(2 ,0.270,0.091);
+						cut->SetPoint(3 ,0.304,0.108);
+						cut->SetPoint(4 ,0.339,0.091);
+						cut->SetPoint(5 ,0.368,0.091);
+						cut->SetPoint(6 ,0.353,0.114);
+						cut->SetPoint(7 ,0.318,0.125);
+						cut->SetPoint(8 ,0.293,0.125);
+						cut->SetPoint(9 ,0.263,0.114);
+						cut->SetPoint(10,0.249,0.091);
+					}
+					return cut->IsInside(Ek(T,P),Th(T,P));
 				}//10
 				<<[](WTrack&T,const vector<double>&P){
-					return (Th(T,P)<0.134);
+					static TCutG *cut=nullptr;
+					if(cut==nullptr){
+						cut=new TCutG("kincut11",11);
+						cut->SetVarX("Ek");
+						cut->SetVarY("Theta");
+						cut->SetPoint(1 ,0.248,0.091);
+						cut->SetPoint(2 ,0.268,0.091);
+						cut->SetPoint(3 ,0.304,0.110);
+						cut->SetPoint(4 ,0.343,0.091);
+						cut->SetPoint(5 ,0.379,0.091);
+						cut->SetPoint(6 ,0.365,0.114);
+						cut->SetPoint(7 ,0.320,0.130);
+						cut->SetPoint(8 ,0.292,0.130);
+						cut->SetPoint(9 ,0.253,0.114);
+						cut->SetPoint(10,0.248,0.091);
+					}
+					return cut->IsInside(Ek(T,P),Th(T,P));
 				}
 			)
 			<<[](WTrack&track)->bool{
