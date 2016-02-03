8aa8c0bb62c7b72b1c5f437d115bacdc1c1e5692
alexkernphysiker <kernphysiker@openmailbox.org>
2016-02-03 17:45:18 +0100

    small fixes

--- luminosity_he3eta.cpp
+++ luminosity_he3eta.cpp
@@ -19,15 +19,15 @@ using namespace std;
 using namespace ROOT_data;
 using namespace GnuplotWrap;
 int main(int,char**){
-	Plotter::Instance().SetOutput(ENV(OUTPUT_PLOTS),"he3eta forward");
-	vector<string> histpath_forward={"Histograms","He3Forward_Reonstruction"};
+	Plotter::Instance().SetOutput(ENV(OUTPUT_PLOTS),"he3eta_forward");
+	vector<string> histpath_forward={"Histograms","He3Forward_Reconstruction"};
 	hist mc_norm_fg(MC,"He3eta",histpath_forward,"0-Reference");
 	hist mc_norm_bg1(MC,"He3pi0pi0",histpath_forward,"0-Reference");
 	hist mc_norm_bg2(MC,"He3pi0pi0pi0",histpath_forward,"0-Reference");
-	
-	double MC_events_count=0;for(auto&p: mc_norm_fg)MC_events_count+=p.y;
-	cout<<"Montecarlo evens count of "<<MC_events_count<<" detected."<<endl;
-	
+	{
+		double MC_events_count=0;for(auto&p: mc_norm_fg)MC_events_count+=p.y;
+		cout<<"Montecarlo evens count of "<<MC_events_count<<" detected."<<endl;
+	}
 	hist mc_accepted_fg(MC,"He3eta",histpath_forward,"5-Kinematic cut");
 	if(mc_accepted_fg.count()!=mc_norm_fg.count())
 		throw Exception<decltype(main)>("mc hists size mismatch");
@@ -39,7 +39,6 @@ int main(int,char**){
 		PlotHist().Hist("All MC events",mc_norm_fg).Hist("Forward Tracks",n1).Hist("FPC",n2).Hist("E_{dep} cuts",n3).Hist("Kinematic cuts",mc_accepted_fg)
 			<<"set yrange [0:]"<<"set xlabel 'Q, MeV'"<<"set ylabel 'Events count'";
 	}
-	
 	for(size_t bin_num=0;bin_num<mc_norm_fg.count();bin_num++)if(mc_accepted_fg[bin_num].y>1){
 		hist foreground(MC,"He3eta",histpath_forward,string("MissingMass-Bin-")+to_string(bin_num));
 		hist background1(MC,"He3pi0pi0",histpath_forward,string("MissingMass-Bin-")+to_string(bin_num));
