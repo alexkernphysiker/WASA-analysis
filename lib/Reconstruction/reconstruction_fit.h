0e56036f407f0ee20b76acc9eb17b9090a340cce
alexkernphysiker <kernphysiker@openmailbox.org>
2016-02-17 16:42:14 +0100

    reconstruction fix

--- lib/Reconstruction/reconstruction_fit.h
+++ lib/Reconstruction/reconstruction_fit.h
@@ -16,17 +16,13 @@ namespace SimulationDataProcess{
 	using namespace GnuplotWrap;
 	string SimulationDataPath();
 	template<class FITFUNC>
-	void He3ForEtaFit(
-		string&&reconstructionname,pair<double,double> E_range,
-		shared_ptr<IInitialConditions>init,
-		shared_ptr<IParamCheck>filter,
-		RANDOM&R
-	){
+	void He3ForEtaFit(string&&reconstructionname,pair<double,double>&&E_range,shared_ptr<IInitialConditions>init,RANDOM&R){
 		auto params_shown=make_pair(0,2);
-		vector<value<double>> theta_bins=BinsByStep(0.10,0.02,0.13);
+		vector<value<double>> theta_bins=BinsByStep(0.10,0.002,0.13);
 		vector<Distribution2D<double>> E_sp2;
 		for(const value<double>&bin:theta_bins)
-			E_sp2.push_back(Distribution2D<double>(BinsByStep(E_range.first,0.05,E_range.second),BinsByStep(E_range.first,0.05,E_range.second)));
+			E_sp2.push_back(Distribution2D<double>(BinsByStep(E_range.first,0.005,E_range.second),BinsByStep(E_range.first,0.005,E_range.second)));
+		cout<<theta_bins.size()<<" theta bins"<<endl;
 		{
 			ifstream file;
 			file.open(SimulationDataPath()+reconstructionname+".simulation.txt");
@@ -36,7 +32,7 @@ namespace SimulationDataProcess{
 				while(getline(file,line)){
 					istringstream str(line);
 					double Edep,theta,Ekin;
-					str>>Edep,theta,Ekin;
+					str>>Edep>>theta>>Ekin;
 					for(size_t i=0,n=theta_bins.size();i<n;i++)
 						if(theta_bins[i].contains(theta))
 							E_sp2[i]<<make_pair(Edep,Ekin);
@@ -53,9 +49,11 @@ namespace SimulationDataProcess{
 				if(!P.Z().contains(0))
 					points<<Point({P.X().val(),theta_bins[i].val()},P.Y().val(),P.Z().val());
 			});
+		cout<<points->size()<<" points"<<endl;
 		FitFunction<DifferentialMutations<>,FITFUNC,SumWeightedSquareDiff> fit(points);
 		cout<<"Init2"<<endl;
-		fit.SetFilter(filter).Init(25*FITFUNC::ParamCount,init,R);
+		fit.Init(25*FITFUNC::ParamCount,init,R);
+		cout<<"population "<<fit.PopulationSize()<<endl;
 		cout<<"Fitting"<<endl;
 		while(
 			(!fit.AbsoluteOptimalityExitCondition(0.000001))&&
@@ -78,8 +76,8 @@ namespace SimulationDataProcess{
 			PlotDistribution2D<double>(sp2).Distr(E_sp2[i],"Theta=["+to_string(theta_bins[i].min())+":"+to_string(theta_bins[i].max())+"]");
 			typedef PlotDistribution2D<double>::point P;
 			vector<P> func_line;
-			for(const value<double>Ed:BinsByStep(E_range.first,0.05,E_range.second))
-				func_line.push_back(P(Ed.val(),fit({Ed.val(),theta_bins[i].val()}),0));
+			for(const value<double>Ed:BinsByStep(E_range.first,0.001,E_range.second))
+				func_line.push_back(P(Ed.val(),fit({Ed.val(),theta_bins[i].val()}),500));
 			PlotDistribution2D<double>(normal).Distr(E_sp2[i],"Theta=["+to_string(theta_bins[i].min())+":"+to_string(theta_bins[i].max())+"]")
 				.Line(func_line,"Theta="+to_string(theta_bins[i].val()));
 		}
--- reconstruction_he3.cpp
+++ reconstruction_he3.cpp
@@ -15,25 +15,21 @@ using namespace GnuplotWrap;
 RANDOM engine;
 int main(int,char**){
   	Plotter::Instance().SetOutput(SimulationDataProcess::SimulationDataPath(),"He3Ekin");
-	Plotter::Instance()<<"set xrange [0:0.4]"<<"set yrange [0:0.6]";
 	SimulationDataProcess::He3ForEtaFit<He3EnergyFRH1>("He3.E.FRH1",make_pair(0,0.6),
 		make_shared<GenerateByGauss>()
 			<<make_pair(0,0.1)<<make_pair(0,0.1)<<make_pair(0,0.1)<<make_pair(0,0.01)
 			<<make_pair(1,0.5)<<make_pair(0,0.1)<<make_pair(0,0.1)<<make_pair(0,0.01)
 			<<make_pair(0,0.1)<<make_pair(0,0.1)<<make_pair(0,0.1)<<make_pair(0,0.01)
 			<<make_pair(0,0.01)<<make_pair(0,0.01)<<make_pair(0,0.01)<<make_pair(0,0.01)
-		,make_shared<Filter>([](const ParamSet&){return true;}),
-		engine
+		,engine
 	);
-	Plotter::Instance()<<"set xrange [0.2:0.6]"<<"set yrange [0.2:0.6]";
-	SimulationDataProcess::He3ForEtaFit<He3EnergyFRH2>("He3.E.FRH2",make_pair(0.25,0.6),
+	SimulationDataProcess::He3ForEtaFit<He3EnergyFRH2>("He3.E.FRH2",make_pair(0.3,0.6),
 		make_shared<GenerateByGauss>()
 			<<make_pair(0,0.1)<<make_pair(0,0.1)<<make_pair(0,0.1)<<make_pair(0,0.01)
 			<<make_pair(1,0.5)<<make_pair(0,0.1)<<make_pair(0,0.1)<<make_pair(0,0.01)
 			<<make_pair(0,0.1)<<make_pair(0,0.1)<<make_pair(0,0.1)<<make_pair(0,0.01)
 			<<make_pair(0,0.01)<<make_pair(0,0.01)<<make_pair(0,0.01)<<make_pair(0,0.01)
-		,make_shared<Filter>([](const ParamSet&){return true;}),
-		engine
+		,engine
 	);
 	
 }
