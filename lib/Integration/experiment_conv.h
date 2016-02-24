778f3a82b5ebcb04cf462ee43de7bffb726e25f4
alexkernphysiker <kernphysiker@openmailbox.org>
2016-02-24 15:40:28 +0100

    trigger changed

--- lib/Integration/experiment_conv.h
+++ lib/Integration/experiment_conv.h
@@ -7,7 +7,7 @@ const double p_beam_low=1.426;
 const double p_beam_hi=1.635;
 const double pbeam_measurement_offset=0.0;
 struct trigger{unsigned char number; unsigned long scaling;};
-const trigger trigger_he3_forward={.number=17,.scaling=4000};
+const trigger trigger_he3_forward={.number=10,.scaling=1};
 
 //calsulational designations
 inline double NormPhi(double p){
