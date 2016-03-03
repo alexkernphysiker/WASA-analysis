7dfef73cb77d64b0561fc401e02d4a8481a896d9
alexkernphysiker <kernphysiker@openmailbox.org>
2016-03-03 23:03:34 +0100

    beam_momenta_offset fit

--- He3Eta/beam_momenta_offset.cpp
+++ He3Eta/beam_momenta_offset.cpp
@@ -7,6 +7,8 @@
 #include <gnuplot_wrap.h>
 #include <math_h/functions.h>
 #include <math_h/error.h>
+#include <Genetic/fit.h>
+#include <Genetic/initialconditions.h>
 #include <Experiment/experiment_conv.h>
 #include <Experiment/str_get.h>
 #include <Experiment/gethist.h>
@@ -16,6 +18,7 @@ using namespace std;
 using namespace ROOT_data;
 using namespace MathTemplates;
 using namespace GnuplotWrap;
+using namespace Genetic;
 const Reaction&main_reaction(){
 	static Reaction main_react(Particle::p(),Particle::d(),{Particle::he3(),Particle::eta()});
 	return main_react;
@@ -39,43 +42,48 @@ int main(){
 			),
 		to_string(P.X().val())
 		);
+	RANDOM engine;
+	vector<point<double>> offs_vertex,offs_mc,offs_data;
 	for(size_t bin_num=9,bin_count=QBins.size();bin_num<bin_count;bin_num++){
 		auto x=QBins[bin_num].X();
-		auto do_plot=[&x,&Q2P](const hist2d<double>&kin_){
-			vector<pair<double,double>> points;
-			double max=0;kin_.FullCycle([&max](point3d<double>&&P){
-				if(max<P.Z().val())
-					max=P.Z().val();
+		auto do_fit=[&engine,&x,&Q2P](const hist2d<double>&kin_)->point<double>{
+			auto points=make_shared<FitPoints>();
+			double max=0;
+			kin_.FullCycle([&max](point3d<double>&&P){
+				if(max<P.Z().val())max=P.Z().val();
 			});
 			kin_.FullCycle([max,&points](point3d<double>&&P){
-				if(P.Z().val()>(2.0*max/3.0))
-					points.push_back(make_pair(P.X().val(),P.Y().val()));
+				if((P.Z().val()>(2.0*max/3.0))&&(P.X().val()>0.25)&&(P.X().val()<0.35))
+					points<<Point({P.X().val()},P.Y().val(),P.Z().val());
 			});
+			Fit<DifferentialMutations<>,SumWeightedSquareDiff> fit(
+				points,
+				[&x,&Q2P](const ParamSet&E,const ParamSet&P){
+					return main_reaction().PbEr2Theta(Q2P((x.val()+P[0])/1000.0),E[0])*180./3.1415926;
+				}
+			);
+			fit.Init(50,make_shared<GenerateUniform>()<<make_pair(-10.0,10.0),engine);
+			while(!fit.AbsoluteOptimalityExitCondition(0.000001))
+				fit.Iterate(engine);
 			Plot<double> kin_plot;
-			kin_plot.Points(points);
-			for(double offset=0.000;offset<0.010;offset+=0.001)
-				kin_plot.Line(
-					LinearInterpolation<double>(
-						[&offset,&x,&Q2P](double E)->double{
-							return main_reaction().PbEr2Theta(Q2P(x.val()/1000.0+offset),E)*180./3.1415926;
-						},
-						ChainWithStep(0.2,0.001,0.4)
-					),
-					to_string(offset)
-				);
+			kin_plot.Points(points->Hist1(0).Line()).Line(
+				LinearInterpolation<double>(
+					[&fit](double E)->double{return fit({E});},
+					ChainWithStep(0.2,0.001,0.4)
+				)
+			);
+			return point<double>(x,value<double>(fit[0],0));
 		};
-		auto kin_v=Hist2d(MC,he3eta,histpath_forward,string("Kinematic-vertex-Bin-")+to_string(bin_num));
-		kin_v=kin_v.Scale(4,4);
+		auto kin_v=Hist2d(MC,he3eta,histpath_forward,string("Kinematic-vertex-Bin-")+to_string(bin_num)).Scale(4,4);
 		PlotHist2d<double>(sp2).Distr(kin_v)<<"set xlabel 'E_k, GeV'"<<"set ylabel 'theta, deg'";
-		do_plot(kin_v);
-		auto kin_mc=Hist2d(MC,he3eta,histpath_forward,string("Kinematic-before-cut-Bin-")+to_string(bin_num));
-		kin_mc=kin_mc.Scale(4,4);
+		offs_vertex.push_back(do_fit(kin_v));
+		auto kin_mc=Hist2d(MC,he3eta,histpath_forward,string("Kinematic-before-cut-Bin-")+to_string(bin_num)).Scale(4,4);
 		PlotHist2d<double>(sp2).Distr(kin_mc)<<"set xlabel 'E_k, GeV'"<<"set ylabel 'theta, deg'";
-		do_plot(kin_mc);
-		auto kin_data=Hist2d(DATA,"He3",histpath_forward,string("Kinematic-before-cut-Bin-")+to_string(bin_num));
-		kin_data=kin_data.Scale(4,4);
+		offs_mc.push_back(do_fit(kin_mc));
+		auto kin_data=Hist2d(DATA,"He3",histpath_forward,string("Kinematic-before-cut-Bin-")+to_string(bin_num)).Scale(4,4);
 		PlotHist2d<double>(sp2).Distr(kin_data);
-		do_plot(kin_data);
+		offs_data.push_back(do_fit(kin_data));
 	}
+	Plot<double>().Hist(offs_vertex,"Vertex").Hist(offs_mc,"WMC").Hist(offs_data,"Data");
 	return 0;
 }
