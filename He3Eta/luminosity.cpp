12be18e9ebb0337c2695bf0ea78ffe3cdc3ef689
alexkernphysiker <kernphysiker@openmailbox.org>
2016-03-25 03:47:31 +0100

    foregroung fit removed. background fit updated.

--- He3Eta/luminosity.cpp
+++ He3Eta/luminosity.cpp
@@ -50,22 +50,24 @@ int main(){
 	Plotter::Instance()<<"unset yrange";
 	vector<hist<double>> acceptance;
 	for(const auto&h:norm)acceptance.push_back(h.CloneEmptyBins());
-	SortedPoints<value<double>> luminosity,bg_chi_sq,fg_chi_sq;
+	SortedPoints<value<double>> luminosity,bg_chi_sq;
 	RANDOM r_eng;
-	for(size_t bin_num=0,bin_count=norm[0].size();bin_num<bin_count;bin_num++){
+	for(size_t bin_num=3,bin_count=norm[0].size();bin_num<bin_count;bin_num++){
 		Plotter::Instance()<<"unset yrange"<<"unset xrange";
-		hist<double> measured=Hist(DATA,"He3",histpath_forward_reconstr,string("MissingMass-Bin-")+to_string(bin_num)).XRange(0.4,0.6);
+		hist<double> measured=Hist(DATA,"He3",histpath_forward_reconstr,string("MissingMass-Bin-")+to_string(bin_num));
+		measured=measured.XRange(0.4,0.7);
 		Plot<double>().Hist(measured,"DATA")<<"set xlabel 'Missing mass, GeV'"<<"set ylabel 'a.u (Q="+to_string(norm[0][bin_num].X().val())+" MeV)'"<<"set yrange [0:]";
 		vector<hist<double>> theory;
 		for(size_t i=0;i<reaction.size();i++){
-			hist<double> react_sim=Hist(MC,reaction[i],histpath_forward_reconstr,string("MissingMass-Bin-")+to_string(bin_num)).XRange(0.4,0.6);
+			hist<double> react_sim=Hist(MC,reaction[i],histpath_forward_reconstr,string("MissingMass-Bin-")+to_string(bin_num));
+			react_sim=react_sim.XRange(0.4,0.7);
 			auto N=value<double>(react_sim.Total());
 			acceptance[i].Bin(bin_num).varY()=N/norm[i][bin_num].Y();
 			theory.push_back(react_sim/norm[i][bin_num].Y());
 		}
 		vector<LinearInterpolation<double>> bg_funcs{theory[1].Line(),theory[2].Line()};
 		Fit<DifferentialMutations<>,ChiSquareWithXError> bg_fit(
-			make_shared<FitPoints>(measured.YRange(750.0,+INFINITY).XRange(0.450,0.542)),
+			make_shared<FitPoints>(measured.XRange(0.50,0.528)),
 			[&bg_funcs](const ParamSet&X,const ParamSet&P){
 				double res=0;
 				for(size_t i=0;i<bg_funcs.size();i++)res+=bg_funcs[i](X[0])*P[i];
@@ -73,8 +75,8 @@ int main(){
 			}
 		);
 		bg_fit.SetUncertaintyCalcDeltas({0.01,0.01}).SetFilter(make_shared<Above>()<<0.0<<0.0);
-		auto count=measured.Total()*50.0;
-		bg_fit.Init(100,make_shared<GenerateUniform>()<<make_pair(0.0,count)<<make_pair(0.0,count),r_eng);
+		auto count=measured.Total()*2.0;
+		bg_fit.Init(40,make_shared<GenerateUniform>()<<make_pair(0.0,count)<<make_pair(0.0,count),r_eng);
 		while(!bg_fit.AbsoluteOptimalityExitCondition(0.000001))
 			bg_fit.Iterate(r_eng);
 		SortedPoints<double> BG_displ(theory[1].Line()*bg_fit[0]+theory[2].Line()*bg_fit[1]);
@@ -88,43 +90,28 @@ int main(){
 		<<"set xlabel 'Missing mass, GeV'"<<"set ylabel 'a.u (Q="+to_string(norm[0][bin_num].X().val())+" MeV)'"<<"set yrange [0:]";
 		
 		Plotter::Instance()<<"unset yrange"<<"unset xrange";
-		auto FG=(measured-BG).XRange(0.50,0.56);
+		auto FG=(measured-BG).XRange(0.45,0.57);
 		Plot<double>().Hist(FG,"substracted").Object("0*x title ''")
 		<<"set xlabel 'Missing mass, GeV'"<<"set ylabel 'a.u (Q="+to_string(norm[0][bin_num].X().val())+" MeV)'"
 		<<"set yrange [-300:1500]";
 		
-		Plotter::Instance()<<"unset yrange"<<"unset xrange";
-		FG=FG.XRange(0.54,0.56).YRange(0.0,+INFINITY);
-		LinearInterpolation<double> fg_mc(theory[0].Line());
-		Fit<DifferentialMutations<>,ChiSquareWithXError> fg_fit(
-			make_shared<FitPoints>(FG),
-			[&fg_mc](const ParamSet&X,const ParamSet&P){
-				return fg_mc(X[0])*P[0];
-			}
-		);
-		fg_fit.SetUncertaintyCalcDeltas({0.01,0.01}).SetFilter(make_shared<Above>()<<0.0)
-		.Init(100,make_shared<GenerateUniform>()<<make_pair(0.0,count),r_eng);
-		while(!fg_fit.AbsoluteOptimalityExitCondition(0.000001))
-			fg_fit.Iterate(r_eng);
-		if(!fg_fit.ParametersWithUncertainties()[0].contains(0)){
-			fg_chi_sq<<point<value<double>>(norm[0][bin_num].X(),fg_fit.Optimality());
-			Plot<double>().Hist(FG,"substracted")
-			.Hist(theory[0].XRange(0.54,0.56)*value<double>(fg_fit[0]),"fit")
-			<<"set xlabel 'Missing mass, GeV'"<<"set ylabel 'a.u (Q="+to_string(norm[0][bin_num].X().val())+" MeV)'"
-			<<"set yrange [0:]";
-			Plot<double>().Hist(FG,"substracted")
-			.Hist(theory[0].XRange(0.54,0.56)*fg_fit.ParametersWithUncertainties()[0],"MC")
+		if(norm[0][bin_num].X().val()>20){
+			Plotter::Instance()<<"unset yrange"<<"unset xrange";
+			FG=FG.XRange(0.52,0.57).YRange(0.0,+INFINITY);
+			value<double> L=0.0;
+			for(const auto&P:FG)L+=P.Y();
+			L/=theory[0].Total();
+			Plot<double>().Hist(FG,"substracted").Hist(theory[0].XRange(0.54,0.56)*L,"MC")
 			<<"set xlabel 'Missing mass, GeV'"
 			<<"set ylabel 'a.u (Q="+to_string(norm[0][bin_num].X().val())+" MeV)'"
 			<<"set yrange [0:]";
 			luminosity<<point<value<double>>(
 				norm[0][bin_num].X(),
-				fg_fit.ParametersWithUncertainties()[0]/func_value(sigmaHe3eta.func(),norm[0][bin_num].X())
+				L/func_value(sigmaHe3eta.func(),norm[0][bin_num].X())
 			);
 		}
 	}
 	Plot<double>().Hist(bg_chi_sq)<<"set xlabel 'Q, MeV'"<<"set ylabel 'BG fit chi^2, n.d.'"<<"set yrange [0:]";
-	Plot<double>().Hist(fg_chi_sq)<<"set xlabel 'Q, MeV'"<<"set ylabel 'FG fit chi^2, n.d.'"<<"set yrange [0:]";
 	{//Plot acceptance
 		Plot<double> plot;
 		plot<<"set yrange [0:0.7]";
