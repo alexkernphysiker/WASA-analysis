72c8adf12d8fd8ed9cdd2c64654140f3675fbe35
alexkernphysiker <kernphysiker@openmailbox.org>
2016-03-27 17:06:43 +0200

    reconstruction fit upgrade

--- He3Eta/reconstruction.cpp
+++ He3Eta/reconstruction.cpp
@@ -15,7 +15,8 @@ using namespace GnuplotWrap;
 RANDOM engine;
 int main(){
   	Plotter::Instance().SetOutput(SimulationDataProcess::SimulationDataPath(),"He3Ekin");
-	SimulationDataProcess::He3ForEtaFit<He3EnergyFRH1>("He3.E.FRH1",
+	SimulationDataProcess::ForwardEkinReconstructionFit(
+		"He3.E.FRH1",make_shared<He3EnergyFRH1>(),
 		BinsByStep(0.0,0.005,0.3),
 		BinsByStep(0.2,0.005,0.5),
 		make_shared<GenerateByGauss>()
@@ -25,7 +26,8 @@ int main(){
 			<<make_pair(0,0.01)<<make_pair(0,0.01)<<make_pair(0,0.01)<<make_pair(0,0.01)
 		,engine
 	);
-	SimulationDataProcess::He3ForEtaFit<He3EnergyFRH2>("He3.E.FRH2",
+	SimulationDataProcess::ForwardEkinReconstructionFit(
+		"He3.E.FRH2",make_shared<He3EnergyFRH2>(),
 		BinsByStep(0.25,0.005,0.45),
 		BinsByStep(0.45,0.005,0.60),
 		make_shared<GenerateByGauss>()
