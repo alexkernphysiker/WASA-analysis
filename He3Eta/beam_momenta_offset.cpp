96d3826825f3e350f7fac7d1f5f19fdc1340c949
alexkernphysiker <kernphysiker@openmailbox.org>
2016-04-09 17:54:43 +0200

    beam momenta offset calculation: plot of data kinematic histogram before energy correction added

--- He3Eta/beam_momenta_offset.cpp
+++ He3Eta/beam_momenta_offset.cpp
@@ -21,10 +21,10 @@ int main(){
 	Plotter::Instance().SetOutput(ENV(OUTPUT_PLOTS),"beam_momenta");
 	Reaction He3eta(Particle::p(),Particle::d(),{Particle::he3(),Particle::eta()});
 	LinearInterpolation<double> Q2P=SortedPoints<double>(
-		[&He3eta](double p){return He3eta.P2Q(p);},ChainWithStep(0.0,0.001,3.0)
+		[&He3eta](double p){return He3eta.P2Q(p);},ChainWithStep(0.0,0.01,3.0)
 	).Transponate();
 	LinearInterpolation<double> Q2E=SortedPoints<double>(
-		[&He3eta](double e){return He3eta.E2Q(e);},ChainWithStep(0.0,0.001,3.0)
+		[&He3eta](double e){return He3eta.E2Q(e);},ChainWithStep(0.0,0.01,3.0)
 	).Transponate();
 	LinearInterpolation<double> ThetaMax2P=SortedPoints<double>(
 		[&He3eta](double p){
@@ -34,9 +34,9 @@ int main(){
 		},ChainWithStep(He3eta.PThreshold(),0.001,3.0)
 	).Transponate();
 	string he3eta="He3eta";
-	SortedPoints<value<double>> offs_mc,offs_data,ek_mc,ek_data;
+	SortedPoints<value<double>> offs_mc,offs_data,ek_mc,ek_data,ek_before_shift;
 	auto QBins=Hist(MC,he3eta,{"Histograms","He3Forward_Reconstruction"},"0-Reference");
-	for(size_t bin_num=8,bin_count=QBins.size();bin_num<bin_count;bin_num++){
+	for(size_t bin_num=9,bin_count=QBins.size();bin_num<bin_count;bin_num++){
 		auto Q=QBins[bin_num].X();
 		double p=Q2P(Q.val()/1000.0);
 		auto P_offset=[&He3eta,&ThetaMax2P,&Q,&p](const hist2d<double>&kin_,const double ratio){
@@ -84,16 +84,39 @@ int main(){
 			});
 			return point<value<double>>(Q,ek_avr());
 		};
-		auto kin_v=Hist2d(MC,he3eta,{"Histograms","He3Forward_Vertices"},string("Kinematic-vertex-Bin-")+to_string(bin_num)).Scale(6,6);
-		PlotHist2d<double>(sp2).Distr(kin_v)<<"set xlabel 'E_k, GeV'"<<"set ylabel 'theta, deg'";
-		auto kin_mc=Hist2d(MC,he3eta,{"Histograms","He3Forward_Reconstruction"},string("Kinematic-reconstructed-Bin-")+to_string(bin_num)).Scale(6,6);
-		PlotHist2d<double>(sp2).Distr(kin_mc)<<"set xlabel 'E_k, GeV'"<<"set ylabel 'theta, deg'";
-		auto kin_data=Hist2d(DATA,"He3",{"Histograms","He3Forward_Reconstruction"},string("Kinematic-reconstructed-Bin-")+to_string(bin_num)).Scale(6,6);
-		PlotHist2d<double>(sp2).Distr(kin_data);
-		offs_mc<<P_offset(kin_mc,2.0/3.0);
-		offs_data<<P_offset(kin_data,2.0/3.0);
-		ek_mc<<Ek_avr(kin_mc,2.0/3.0);
-		ek_data<<Ek_avr(kin_data,2.0/3.0);
+		auto kin_v=Hist2d(MC,he3eta,{"Histograms","He3Forward_Vertices"},string("Kinematic-vertex-Bin-")+to_string(bin_num)).Scale(3,3);
+		PlotHist2d<double>(sp2).Distr(kin_v) << "set xlabel 'E_k, GeV'" << "set ylabel 'theta, deg'"
+		<< "set xrange [0.2:0.4]";
+		auto kin_mc=Hist2d(MC,he3eta,{"Histograms","He3Forward_Reconstruction"},string("Kinematic-reconstructed-Bin-")+to_string(bin_num)).Scale(3,3);
+		PlotHist2d<double>(sp2).Distr(kin_mc)<<"set xlabel 'E_k, GeV'"<<"set ylabel 'theta, deg'"
+		<< "set xrange [0.2:0.4]";
+		auto data_hist_init=Hist2d(DATA,"He3",{"Histograms","He3Forward_Reconstruction"},string("Kinematic-reconstructed-Bin-")+to_string(bin_num));
+		{
+			hist2d<double> shifted_back(
+				BinsByCount(data_hist_init.X().size(),
+					    data_hist_init.X().left().min()-he3_forward_correct_energy,
+					    data_hist_init.X().right().max()-he3_forward_correct_energy
+				),
+				BinsByCount(data_hist_init.Y().size(),
+					    data_hist_init.Y().left().min(),
+					    data_hist_init.Y().right().max()
+				)
+			);
+			for(size_t x=0;x<shifted_back.X().size();x++)
+				for(size_t y=0;y<shifted_back.Y().size();y++)
+					shifted_back.Bin(x,y)=data_hist_init[x][y];
+			shifted_back=shifted_back.Scale(3,3);
+			PlotHist2d<double>(sp2).Distr(shifted_back)<<"set xlabel 'E_k, GeV'"<<"set ylabel 'theta, deg'"
+			<< "set xrange [0.2:0.4]";
+			ek_before_shift << Ek_avr(shifted_back,3.0/4.0);
+		}
+		auto kin_data=data_hist_init.Scale(3,3);
+		PlotHist2d<double>(sp2).Distr(kin_data)<<"set xlabel 'E_k, GeV'"<<"set ylabel 'theta, deg'"
+		<< "set xrange [0.2:0.4]";
+		offs_mc << P_offset(kin_mc,3.0/4.0);
+		offs_data << P_offset(kin_data,3.0/4.0);
+		ek_mc << Ek_avr(kin_mc,3.0/4.0);
+		ek_data << Ek_avr(kin_data,3.0/4.0);
 	}
 	Plot<double>().Hist(offs_mc,"WMC").Hist(offs_data,"Data")
 		<<"set xlabel 'Q, MeV'"<<"set ylabel 'delta P, GeV/c'";
