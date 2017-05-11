c5c886f26065fd8c40be1800c07459f2c69f39fd
alexkernphysiker <kernphysiker@openmailbox.org>
2017-05-11 21:25:29 +0200

    adding he3 pi0 reaction to analysis

--- He3Eta/forward.cpp
+++ He3Eta/forward.cpp
@@ -22,14 +22,18 @@ using namespace GnuplotWrap;
 int main(){
     Plotter::Instance().SetOutput(ENV(OUTPUT_PLOTS),"he3eta_forward");
     vector<string> histpath_forward_reconstr={"Histograms","He3Forward_Reconstruction"};
-    vector<string> reaction={"He3eta","He3pi0pi0","He3pi0pi0pi0","He3pi0"};
+    vector<string> reaction={"He3eta","He3pi0pi0pi0","He3pi0pi0","He3pi0"};
     vector<hist<double>> norm;
-    for(const string& r:reaction)norm.push_back(Hist(MC,r,histpath_forward_reconstr,"0-Reference"));
+    for(const string& r:reaction)
+	norm.push_back(Hist(MC,r,histpath_forward_reconstr,"0-Reference"));
 
     vector<hist<double>> acceptance;
     for(size_t i=0;i<norm.size();i++)
 	acceptance.push_back(hist<double>());
-    hist<double> luminosity,bg_chi_sq,bg_ratio;
+    hist<double> luminosity,bg_chi_sq;
+    vector<hist<double>> fit_params;
+    for(size_t i=0;i<reaction.size();i++)
+	fit_params.push_back(hist<double>());
     RANDOM r_eng;
     for(size_t bin_num=0,bin_count=norm[0].size();bin_num<bin_count;bin_num++)
 	if(norm[0][bin_num].X()>10.0){
@@ -48,7 +52,7 @@ int main(){
 	    vector<hist<double>> theory;{
 		Plot<double> th_plot;
 		th_plot<< "set key on"<< "set xlabel 'Missing mass, MeV'"
-		<< "set title '"+Qmsg+"'"//<<"set log y"
+		<< "set title '"+Qmsg+"'"<<"set log y"
 		<< "set ylabel 'acceptance density, GeV^{-1}'";
 		for(size_t i=0;i<reaction.size();i++){
 		    hist<double> react_sim=Hist(MC,reaction[i],histpath_forward_reconstr,string("MissingMass-Bin-")+to_string(bin_num));
@@ -64,7 +68,7 @@ int main(){
 	    SearchMin<DifferentialMutations<ParabolicErrorEstimationFromChisq>>
 	    fit([&theory,&data](const ParamSet&P){
 		double res=0;
-		for(size_t i=1,n=data.size()-1;i<n;i++){
+		for(size_t i=1,n=data.size();i<n;i++){
 		    value<double> exp_p=data[i].Y(),the_p=0;
 		    for(size_t j=0,n=theory.size()-1;j<n;j++)
 			the_p+=theory[j][i].Y()*P[j];
@@ -74,13 +78,14 @@ int main(){
 	    });
 	    const auto ex=parEq(3,0.001);
 	    fit.SetUncertaintyCalcDeltas(ex)
-	    .SetFilter(make_shared<Above>()<<0.0<<0.0<<0.0);
+	    .SetFilter(make_shared<Above>()<<0.0<<0.0<<0.0<<0.0);
 	    const auto&data_count=data.TotalSum().val();
 	    fit.Init(80,
 		make_shared<InitialDistributions>()
 		<<make_shared<DistribUniform>(0.0,2.0*data_count)
 		<<make_shared<DistribUniform>(0.0,2.0*data_count)
-		<<make_shared<DistribUniform>(0.0,2.0*data_count),
+		<<make_shared<DistribUniform>(0.0,2.0*data_count)
+		<<make_shared<DistribUniform>(0.0,0.5*data_count),
 		r_eng
 	    );
 	    while(
@@ -93,7 +98,8 @@ int main(){
 		<<"          \r";
 	    }
 	    const auto&P=fit.ParametersWithUncertainties();
-	    bg_ratio << point<value<double>>(Q,(P[2]/P[1])/(acceptance[1].right().Y()/acceptance[2].right().Y()));
+	    for(size_t i=0;i<reaction.size();i++)
+		fit_params[i]<< point<value<double>>(Q,P[i]);
 	    bg_chi_sq << point<value<double>>(Q,fit.Optimality()/(data.size()-fit.ParamCount()));
 	    exp_plot
 	    .Line(hist<double>(theory[0]*P[0]+theory[1]*P[1]+theory[2]*P[2]).toLine(),"Total")
@@ -111,27 +117,29 @@ int main(){
     << "set yrange [0:]"<<"unset log y";
 
     {//Plot acceptance
-	Plot<double> plot;
-	plot << "set key on" 
+	Plot<double> acc,par;
+	acc << "set key on" 
 	<< "set title 'Acceptance'"
-	<< "set yrange [0:1.0]"
+	<< "set yrange [0:1.0]"<<"unset log y"
 	<< "set xlabel 'Q, MeV'" 
 	<< "set ylabel 'Acceptance, n.d.'";
-	for(size_t i=0;i<reaction.size();i++)plot.Hist(acceptance[i],reaction[i]);
+	par << "set key on" 
+	<< "set title 'Fitted coefficients'"
+	<< "set yrange [0:]"
+	<< "set xlabel 'Q, MeV'" 
+	<< "set ylabel 'coefficient, n.d.'";
+	for(size_t i=0;i<reaction.size();i++){
+	    acc.Hist(acceptance[i],reaction[i]);
+	    par.Hist(fit_params[i],reaction[i]);
+	}
     }
-
-    Plot<double>().Hist(bg_ratio) 
-    << "set title 'sigma("+reaction[1]+")/sigma("+reaction[2]+")'"
-    << "set xlabel 'Q, MeV'" 
-    << "set ylabel 'n.d.'" 
-    << "set yrange [0:]";
 
     Plot<double>()
     .Hist(hist<double>(he3eta_sigma().func(),BinsByStep(10.0,2.5,30.0)))
     .Hist(he3eta_sigma(),"Data from other experiments")
     << "set title 'Cross section of "+reaction[0]+" used in the calculations'"
     << "set key on" << "set xlabel 'Q, MeV'" 
-    << "set ylabel 'sigma(^3He eta), nb'"
+    << "set ylabel 'sigma(^3He eta), nb'"<<"unset log y"
     << "set xrange [0:45]"<< "set yrange [0:600]";
 
     auto runs=PresentRuns("");
