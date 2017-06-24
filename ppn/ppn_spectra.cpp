5f631413301a83240f8319bfe0f138c733b175b8
alexkernphysiker <kernphysiker@openmailbox.org>
2017-06-24 12:46:21 +0200

    ppn spectra fix

--- ppn/ppn_spectra.cpp
+++ ppn/ppn_spectra.cpp
@@ -16,28 +16,29 @@ using namespace GnuplotWrap;
 int main(){
     Plotter::Instance().SetOutput(ENV(OUTPUT_PLOTS),"ppn");
     Plot<double>()
-    .Hist(Hist(MC,"pd",{"Histograms","elastic"},"Compl_mag_0"),"pd")
-    .Hist(Hist(MC,"ppn_qf",{"Histograms","elastic"},"Compl_mag_0"),"ppn_qf")<<"set key on";
+    .Hist(Hist(MC,"pd",{"Histograms","elastic"},"pair_phi_diff_0"),"pd")
+    .Hist(Hist(MC,"ppn_qf",{"Histograms","elastic"},"pair_phi_diff_0"),"ppn_qf")<<"set key on";
     Plot<double>()
-    .Hist(Hist(MC,"pd",{"Histograms","elastic"},"Compl_mag_2"),"pd")
-    .Hist(Hist(MC,"ppn_qf",{"Histograms","elastic"},"Compl_mag_2"),"ppn_qf")<<"set key on";
+    .Hist(Hist(MC,"pd",{"Histograms","elastic"},"pair_phi_diff_1"),"pd")
+    .Hist(Hist(MC,"ppn_qf",{"Histograms","elastic"},"pair_phi_diff_1"),"ppn_qf")<<"set key on";
     Plot<double>()
-    .Hist(Hist(DATA,"L",{"Histograms","elastic"},"Compl_mag_0"))
-    .Hist(Hist(DATA,"L",{"Histograms","elastic"},"Compl_mag_1"))
-    .Hist(Hist(DATA,"L",{"Histograms","elastic"},"Compl_mag_2"));
+    .Hist(Hist(DATA,"L",{"Histograms","elastic"},"pair_phi_diff_0"))
+    .Hist(Hist(DATA,"L",{"Histograms","elastic"},"pair_phi_diff_1"));
     PlotHist2d<double>(sp2).Distr(Hist2d(MC,"pd",{"Histograms","elastic"},"t_vs_t_1"))
     <<"set zrange [0:]";
     PlotHist2d<double>(sp2).Distr(Hist2d(MC,"ppn_qf",{"Histograms","elastic"},"t_vs_t_1"))
-    <<"set zrange [0:800]";
-    PlotHist2d<double>(sp2).Distr(Hist2d(MC,"pd",{"Histograms","elastic"},"t_vs_t_2"))
     <<"set zrange [0:]";
-    PlotHist2d<double>(sp2).Distr(Hist2d(MC,"ppn_qf",{"Histograms","elastic"},"t_vs_t_2"))
-    <<"set zrange [0:800]";
+    PlotHist2d<double>(sp2).Distr(Hist2d(MC,"pd",{"Histograms","elastic"},"t_vs_t_21"))
+    <<"set zrange [0:]";
+    PlotHist2d<double>(sp2).Distr(Hist2d(MC,"ppn_qf",{"Histograms","elastic"},"t_vs_t_21"))
+    <<"set zrange [0:]";
     PlotHist2d<double>(sp2).Distr(Hist2d(DATA,"L",{"Histograms","elastic"},"t_vs_t_1"))
-    <<"set zrange [0:800]";
-    PlotHist2d<double>(sp2).Distr(Hist2d(DATA,"L",{"Histograms","elastic"},"t_vs_t_2"))
-    <<"set zrange [0:800]";
+    <<"set zrange [0:]";
+    PlotHist2d<double>(sp2).Distr(Hist2d(DATA,"L",{"Histograms","elastic"},"t_vs_t_21"))
+    <<"set zrange [0:]";
     Plot<double>()
-    .Hist(Hist(DATA,"L",{"Histograms","elastic"},"theta_sum_1"))
-    .Hist(Hist(DATA,"L",{"Histograms","elastic"},"theta_sum_2"));
+    .Hist(Hist(MC,"pd",{"Histograms","elastic"},"theta_sum_21"))
+    .Hist(Hist(MC,"ppn_qf",{"Histograms","elastic"},"theta_sum_21"));
+    Plot<double>()
+    .Hist(Hist(DATA,"L",{"Histograms","elastic"},"theta_sum_21"));
 }
