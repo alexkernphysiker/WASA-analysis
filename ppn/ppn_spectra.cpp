1277c0776670e2d0056b2c9da2cefa3a15ee736c
alexkernphysiker <kernphysiker@openmailbox.org>
2017-07-18 16:14:41 +0200

    first estimation of ppn_qf events count

--- ppn/ppn_spectra.cpp
+++ ppn/ppn_spectra.cpp
@@ -7,11 +7,16 @@
 #include <memory>
 #include <gnuplot_wrap.h>
 #include <math_h/interpolate.h>
+#include <Genetic/searchmin.h>
+#include <Genetic/initialconditions.h>
+#include <Genetic/filter.h>
+#include <Genetic/parabolic.h>
 #include <Experiment/experiment_conv.h>
 #include <Experiment/str_get.h>
 #include <Experiment/gethist.h>
 using namespace std;
 using namespace ROOT_data;
+using namespace Genetic;
 using namespace MathTemplates;
 using namespace GnuplotWrap;
 int main(){
@@ -64,14 +69,15 @@ int main(){
     Plot<>()
     .Line(Hist(MC,"pd",{"Histograms","elastic"},"theta_sum_22-AllBins").toLine()/norm_pd.TotalSum().val(),"pd")
     .Line(Hist(MC,"ppn_qf",{"Histograms","elastic"},"theta_sum_22-AllBins").toLine()/norm.TotalSum().val(),"ppn_{sp}")
-    <<"set title 'MC'"<<"set key on"<<"set yrange [0:]"<<"set xrange [0:300]"<<"set xlabel "+thth;
+    <<"set title 'MC'"<<"set key on"<<"set yrange [0:]"<<"set xlabel "+thth<<"set xrange [70:200]";
     Plot<>()
-    .Hist(Hist(DATA,"E",{"Histograms","elastic"},"theta_sum_22-AllBins"))
-    <<"set title 'Data "+runmsg+"'"<<"set key on"<<"set yrange [0:]"<<"set xrange [0:300]"<<"set xlabel "+thth;
+    .Hist(Hist(DATA,"E",{"Histograms","elastic"},"theta_sum_22-AllBins"))<<"set xrange [70:200]"
+    <<"set title 'Data "+runmsg+"'"<<"set key on"<<"set yrange [0:]"<<"set xlabel "+thth;
 
 
-
-    hist<> acceptance,acceptance_pd;
+    RANDOM r_eng;
+    hist<> acceptance,acceptance_pd,chi_sq,luminosity;
+    vector<hist<double>> fit_params{hist<double>(),hist<double>(),hist<double>(),hist<double>()};
     for(size_t bin_num=0,bin_count=norm.size();bin_num<bin_count;bin_num++){
 	const auto&Q=norm[bin_num].X();
 	const auto&N=norm[bin_num].Y();
@@ -81,24 +87,78 @@ int main(){
 	    <<Q.min()<<"; "<<Q.max()<<"] MeV"
 	).str();
 
-	const hist<> mc_ppn=Hist(MC,"ppn_qf",{"Histograms","elastic"},string("theta_sum_22-Bin-")+to_string(bin_num));
-	const hist<> mc_pd=Hist(MC,"pd",{"Histograms","elastic"},string("theta_sum_22-Bin-")+to_string(bin_num));
+	const hist<> mc_ppn=Hist(MC,"ppn_qf",{"Histograms","elastic"},string("theta_sum_22-Bin-")+to_string(bin_num)).XRange(80,200);
+	const hist<> mc_pd=Hist(MC,"pd",{"Histograms","elastic"},string("theta_sum_22-Bin-")+to_string(bin_num)).XRange(80,200);
 	const hist<> nmc_ppn=mc_ppn/(N*mc_ppn[0].X().uncertainty()*2.);
 	const hist<> nmc_pd=mc_pd/(N_pd*mc_pd[0].X().uncertainty()*2.);
 	acceptance<<point<value<>>(Q,mc_ppn.TotalSum()/N);
 	acceptance_pd<<point<value<>>(Q,mc_pd.TotalSum()/N_pd);
-	const hist<> data=Hist(DATA,"E",{"Histograms","elastic"},string("theta_sum_22-Bin-")+to_string(bin_num));
-
+	const hist<> data=Hist(DATA,"E",{"Histograms","elastic"},string("theta_sum_22-Bin-")+to_string(bin_num)).XRange(80,200);
 	Plot<>().Hist(nmc_ppn,"ppn_{sp}").Hist(nmc_pd,"pd")
 	<<"set key on"<<"set title 'MC "+Qmsg+"'"<<"set yrange [0:0.015]"
 	<<"set xlabel "+thth<<"set ylabel 'Differential acceptance, deg^{-1}'";
-	Plot<>()
-	.Hist(data,"data")
+	
+	SearchMin<DifferentialMutations<Uncertainty>>
+	fit([&nmc_pd,&nmc_ppn,&data](const ParamSet&P){
+		double res=0;
+		for(size_t i=1,n=data.size();i<n;i++){
+		    const value<double> exp_p=data[i].Y(),
+		    the_p=nmc_ppn[i].Y()*P[2]+nmc_pd[i].Y()*P[3]+P[0]+data[i].X().val()*P[1];
+		    res+=exp_p.NumCompare(the_p);
+		}
+		return res;
+	});
+	fit.SetMutationCoefficient(0.8);
+	fit.SetFilter([](const ParamSet&P)->bool{return (P[2]>0)&&(P[3]>=0);});
+	const auto&data_count=data.TotalSum().val();
+	fit.Init(100,
+	    make_shared<InitialDistributions>()
+		<<make_shared<DistribGauss>(0.0,0.02*data_count)
+		<<make_shared<DistribGauss>(0.0,0.02*data_count)
+		<<make_shared<DistribUniform>(0.0,2.0*data_count)
+		<<make_shared<DistribUniform>(0.0,2.0*data_count)
+	    ,r_eng
+	);
+	while(!fit.AbsoluteOptimalityExitCondition(0.000000000001)){
+	    fit.Iterate(r_eng);
+	    cout<<fit.iteration_count()<<" iterations; "
+	    <<fit.Optimality()<<"<chi^2<"
+	    <<fit.Optimality(fit.PopulationSize()-1)
+	    <<"          \r";
+	}
+	fit.SetUncertaintyCalcDeltas({0.1,0.1,0.1,0.1});
+	const auto&P=fit.ParametersWithUncertainties();
+	for(size_t i=0;i<fit_params.size();i++){
+	    fit_params[i]<< point<value<double>>(Q,P[i]);
+	}
+	chi_sq << point<value<double>>(Q,fit.Optimality()/(data.size()-fit.ParamCount()));
+	const SortedPoints<> PPN=nmc_ppn.toLine()*fit.Parameters()[2],PD=nmc_pd.toLine()*fit.Parameters()[3],
+	BG=SortedPoints<>(data.toLine()).Transform([&fit](const double&x,const double&){return fit.Parameters()[0]+fit.Parameters()[1]*x;});
+	Plot<>().Hist(data,"data").Line(PPN,"ppn").Line(PD,"pd").Line(BG,"background")
+	.Line(PPN+PD+BG,"total")
 	<<"set key on"<<"set title 'Data "+Qmsg+" "+runmsg+"'"<<"set yrange [0:]"
 	<<"set xlabel "+thth<<"set ylabel 'Count'";
-
+	
+	luminosity << point<value<double>>(Q,
+	    (P[0]/1.0)//ToDo: divide by cross section
+	    *double(trigger_elastic.scaling)
+	);
 
     }
     Plot<>().Hist(acceptance,"ppn_{sp}").Hist(acceptance_pd,"pd")<<"set key on"
     <<"set title 'Acceptance'"<<"set yrange [0:]"<<"set xlabel 'Q, MeV'"<<"set ylabel 'Acceptance, n.d.'";
+    for(size_t i=0;i<fit_params.size();i++)
+	Plot<double>().Hist(fit_params[i])
+	<< "set xlabel 'Q, MeV'" 
+	<< "set ylabel 'parameter"+to_string(i)+"'";
+    Plot<double>().Hist(chi_sq)
+    << "set xlabel 'Q, MeV'" 
+    << "set ylabel 'chi^2/d, n.d.'" 
+    << "set yrange [0:]"<<"unset log y";
+    
+    Plot<double>().Hist(luminosity) 
+    << "set title 'Integrated luminosity estimation ("+runmsg+")'"
+    << "set key on" << "set xlabel 'Q, MeV'" 
+    << "set ylabel 'Events count, n.d.'" 
+    << "set xrange [-70:30]"<< "set yrange [0:]";
 }
