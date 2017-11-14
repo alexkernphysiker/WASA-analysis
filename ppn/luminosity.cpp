f2479e1c9ec6a9a611f4c61d1bde74ee38dd8b69
rundel <alexrndl@gmail.com>
2017-11-14 16:28:10 +0100

    minor fix

--- ppn/luminosity.cpp
+++ ppn/luminosity.cpp
@@ -351,6 +351,7 @@ int main()
             << "set key on" << "set xlabel 'Q, MeV'"
             << "set ylabel 'Integrated luminosity, nb^{-1}'"
             << "set xrange [-70:30]" << "set yrange [0:]";
+    cout<<"Full luminosity estimation: "<<estimate_full_luminosity.TotalSum();
 }
 
 
