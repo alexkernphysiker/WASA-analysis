77f75c2e638caa6251db3352891aba2ada69be93
rundel <alexrndl@gmail.com>
2018-07-19 15:49:10 +0300

    eta angular distribution plots fixed

--- InputDataConvert/he3eta.cpp
+++ InputDataConvert/he3eta.cpp
@@ -22,16 +22,16 @@ int main()
         {0.112,-0.10},{0.130,-0.18},{0.145,-0.21},{0.170,-0.29}
     };
     const auto chain=ChainWithStep(0.0,0.001,0.170);
-    Plot("he3eta-alpha").Line(SortedPoints<>(alpha.func(),chain)).Points(alpha)
-        << "set xlabel 'p_{eta,CM}, GeV/c'"<<"set xrange [0:]"
+    Plot("he3eta-alpha",4).Line(SortedPoints<>(alpha.func(),chain)).Points(alpha.XRange(0.1,0.2),"","","with points pointtype 7 pointsize 3")
+        << "set xlabel 'p_{eta,CM}, GeV/c'"<<"set xrange [0:0.2]"
         << "set ylabel 'alpha, n.d.'"<<"set yrange [0:1.5]"
         << "set title 'Alpha parameter'";
-    Plot("he3eta-beta").Line(SortedPoints<>(beta.func(),chain)).Points(beta)
-        << "set xlabel 'p_{eta,CM}, GeV/c'"<<"set xrange [0:]"
+    Plot("he3eta-beta",4).Line(SortedPoints<>(beta.func(),chain)).Points(beta.XRange(0.1,0.2),"","","with points pointtype 7 pointsize 3")
+        << "set xlabel 'p_{eta,CM}, GeV/c'"<<"set xrange [0:0.2]"
         << "set ylabel 'beta, n.d.'"<<"set yrange [-0.5:0]"
         << "set title 'Beta parameter'";
-    Plot("he3eta-gamma").Line(SortedPoints<>(gamma.func(),chain)).Points(gamma)
-        << "set xlabel 'p_{eta,CM}, GeV/c'"<<"set xrange [0:]"
+    Plot("he3eta-gamma",4).Line(SortedPoints<>(gamma.func(),chain)).Points(gamma.XRange(0.1,0.2),"","","with points pointtype 7 pointsize 3")
+        << "set xlabel 'p_{eta,CM}, GeV/c'"<<"set xrange [0:0.2]"
         << "set ylabel 'gamma, n.d.'"<<"set yrange [-0.3:0]"
         << "set title 'Gamma parameter'";
 }
